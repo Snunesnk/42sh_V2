f766bc9e433eeda7fe5fb526ccfcd772715b0fd8
Antoine Barthelemy <abarthel@student.42.fr>
2019-09-16 12:33:53 -0400

    exit fixes(builtins): fix bugs related to exit(1), it ws not compliant and was not conform to bash as a reference

--- src/job/builtins/builtin_exit.c
+++ src/job/builtins/builtin_exit.c
@@ -16,53 +16,74 @@
 #include "error.h"
 #include "jcont.h"
 
-int	cmd_exit(int argc, char **argv)
+static int	part_sep(int argc, char **argv)
+{
+	extern char	**environ;
+	unsigned char	status;
+	
+	status = g_retval;
+	if (!ft_strcmp("--", argv[1]))
+	{
+		if (argc == 2)
+		{
+			ft_tabdel(&argv);
+			ft_tabdel(&environ);
+			write(STDOUT_FILENO, "exit\n", 5 * sizeof(char));
+			exit(status);
+		}
+		return (2);
+	}
+	return (1);
+}
+
+static int	numarg_exit(int argc, char **argv, int i)
+{
+	extern char	**environ;
+	unsigned char	status;
+	
+	status = g_retval;
+	write(STDOUT_FILENO, "exit\n", 5 * sizeof(char));
+	if (argc > i + 1)
+	{
+		ft_dprintf(STDERR_FILENO,
+		"%s: %s: too many arguments\n",	g_progname, argv[0]);
+		return (1);
+	}
+	status = (unsigned char)ft_atoi(argv[i]);
+	ft_tabdel(&argv);
+	ft_tabdel(&environ);
+	exit(status);
+}
+
+static void	nomatter_exit(char **argv, int i)
+{
+	extern char	**environ;
+	
+	write(STDOUT_FILENO, "exit\n", 5 * sizeof(char));
+	ft_dprintf(STDERR_FILENO,
+	"%s: %s: %s: numeric argument required\n",
+			g_progname, argv[0], argv[i]);
+	ft_tabdel(&argv);
+	ft_tabdel(&environ);
+	exit(2);
+}
+
+int		cmd_exit(int argc, char **argv)
 {
 	extern char	**environ;
 	unsigned char	status;
 	int		i;
 
-	i = 1;
 	status = g_retval;
 	if (argc > 1)
 	{
-		if (!ft_strcmp("--", argv[i]))
-		{
-			++i;
-			if (argc == i)
-			{
-				ft_tabdel(&argv);
-				ft_tabdel(&environ);
-				write(STDOUT_FILENO, "exit\n", 5 * sizeof(char));
-				exit(status);
-			}
-		}
+		i = part_sep(argc, argv);
 		if (*argv[i]
 			&& (((*argv[i] == '-' || *argv[i] == '+') && ft_str_is_numeric(&argv[i][i]))
 			|| ft_str_is_numeric(argv[i])) && ft_strcmp("--", argv[i]))
-		{
-			write(STDOUT_FILENO, "exit\n", 5 * sizeof(char));
-			if (argc > i + 1)
-			{
-				ft_dprintf(STDERR_FILENO,
-				"%s: %s: too many arguments\n",	g_progname, argv[0]);
-				return (1);
-			}
-			status = (unsigned char)ft_atoi(argv[i]);
-			ft_tabdel(&argv);
-			ft_tabdel(&environ);
-			exit(status);
-		}
+			return (numarg_exit(argc, argv, i));
 		else
-		{
-			write(STDOUT_FILENO, "exit\n", 5 * sizeof(char));
-			ft_dprintf(STDERR_FILENO,
-			"%s: %s: %s: numeric argument required\n",
-					g_progname, argv[0], argv[i]);
-			ft_tabdel(&argv);
-			ft_tabdel(&environ);
-			exit(2);
-		}
+			nomatter_exit(argv, i);
 	}
 	ft_tabdel(&argv);
 	ft_tabdel(&environ);
