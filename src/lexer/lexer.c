49d06caa67b79457373e426c91c3b8507c409577
Antoine Barthelemy <abarthel@student.42.fr>
2019-11-29 20:11:47 +0100

    in case dummies

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -72,36 +72,40 @@ static t_token	get_io_number(char *io_nb_start, char *io_nb_end)
 }
 
 /* Analyze the input string and eat the first chunck of that input which correspond to a token */
-t_token	tokenizer(char *input)
+t_token	tokenizer(char **input)
 {
 	t_token	token;
-/*	char	*start; */
+	char	*str;
 	char	*digit_start;
 
-/*	start = input;
-*/	token = (t_token){.symbol = NULL, .type = -1}; /* set to "no type" the token */
-	while (*input == ' ' || *input == '\t') /* skip all useless characters */
-		++input;
-	if (ft_isdigit(*input))
+	str = *input;
+	token = (t_token){.symbol = NULL, .type = -1}; /* set to "no type" the token */
+	while (*str == ' ' || *str == '\t') /* skip all useless characters */
+		++str;
+	if (ft_isdigit(*str))
 	{
-		digit_start = input;
-		while (ft_isdigit(*input)) /* skip digit to know if a <, <<, > or >> follows the digit,
+		digit_start = str;
+		while (ft_isdigit(*str)) /* skip digit to know if a <, <<, > or >> follows the digit,
 					in this case the number is a IO_NUMBER, eg ls 124> toto */
-			++input;
-		if (*input == '>' || *input == '<')
-			token = get_io_number(digit_start, input); /* get the io_number abd return it
+			++str;
+		if (*str == '>' || *str == '<')
+		{
+			token = get_io_number(digit_start, str); /* get the io_number abd return it
 									as token, e.g. ls 2> ok, 2 is io_number */
+			*input = str; /* set input line read to the end of the taken token */
+		}
 	}
 /*	else
 	{
+		token.symbol = get_symbol(&str);
+		token.type = WORD;
 	}
-	token.symbol = get_symbol(input);
-	token.type = WORD;
-*/	return (token);
+*/
+	return (token);
 }
 
 /* malloc a token and set its value before returning it to the lexer */
-t_token	*get_next_token(char *input)
+t_token	*get_next_token(char **input)
 {
 	t_token	*token;
 
@@ -124,9 +128,11 @@ int	lexer(char *input, struct s_queue *queue)
 {
 	t_token	*token;
 
-	while ((token = get_next_token(input))) /* Get a token from the input until no more token */
+	while ((token = get_next_token(&input))) /* Get a token from the input until no more token */
 	{
 		queue_enqueue(queue, token); /* Put that token into a queue to speed up the recursive descent during parsing */
+		ft_printf("symbol: %s\n", token->symbol);/* deebug */
+		ft_printf("type: %d\n\n", token->type);/* deebug */
 	}
 	if (g_errno == E_ENOMEM) /* If a malloc failed, g_errno set error value and then return the corresponding error value */
 		return (e_cannot_allocate_memory);
