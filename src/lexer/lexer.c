1880234d01736bf2a3652684b73376789b434a9a
Antoine Barthelemy <abarthel@student.42.fr>
2019-11-29 18:49:15 +0100

    doc(lexer): add comment to the lexer

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -56,43 +56,54 @@ char	*get_symbol(char **str)
 	return (tmp);
 }
 
+/* Analyze the input string and eat the first chunck of that input which correspond to a token */
 t_token	tokenizer(char *input)
 {
+	t_token	token;
 	char	*start;
+	char	*;
 
 	start = input;
-	while (*input == ' ' || *input == '\t')
+	while (*input == ' ' || *input == '\t') /* skip all useless characters */
 		++input;
-	/* check if atoi then >> > < << etc 
+	while (ft_isdigit(input)) /* skip digit to know if a <, <<, > or >> follows the digit,
+					in this case the number is a IO_NUMBER, eg ls 124> toto */
+		++input;
+	/* check if atoi then >> > < << etc
 	if (input[x] == '<' || input[x] == '>')
 	*/
 	token->symbol = get_symbol(input);
 	token->type = WORD;
+	return (token);
 }
 
+/* malloc a token and set its value before returning it to the lexer */
 t_token	*get_next_token(char *input)
 {
 	t_token	*token;
 
 	token = (t_token*)ft_memalloc(sizeof(t_token));
-	if (!token)
+	if (!token) /* if malloc fails, g_errno value is set */
 	{
 		g_errno = E_ENOMEM;
 		return (NULL);
 	}
-	*token = tokenizer(input);
+	*token = tokenizer(input); /* assign value of the token without memcpy */
 	return (token);
 }
 
+
+/* this function will transform the input into token.
+   token will be put into a queue */
 int	lexer(char *input, struct s_queue *queue)
 {
 	t_token	*token;
 
-	while ((token = get_next_token(input)))
+	while ((token = get_next_token(input))) /* Get a token from the input until no more token */
 	{
-		queue_enqueue(queue, token);
+		queue_enqueue(queue, token); /* Put that token into a queue to speed up the recursive descent during parsing */
 	}
-	if (g_errno == E_ENOMEM)
+	if (g_errno == E_ENOMEM) /* If a malloc failed, g_errno set error value and then return the corresponding error value */
 		return (e_cannot_allocate_memory);
 	return (e_success);
 }
