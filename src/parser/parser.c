9fde11311c4843ee117f36f0a0fba4f7c37d57b4
Antoine Barthelemy <abarthel@student.42.fr>
2019-11-30 15:08:53 +0100

    feat(job control): adding structures for job control

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -7,6 +7,7 @@
 #include "error.h"
 #include "tokens.h"
 #include "grammar.h"
+#include "syntaxic_tree.h"
 
 #include "job.h"
 
@@ -16,20 +17,35 @@ void	display_tokens(t_token *token)
 	ft_printf("type: %d\n\n", token->type);/* deebug */
 }
 
+/*
+int	execute_tree()
+{
+	argv = (char**)ft_memalloc(sizeof(char*) * 2);
+	*argv = token->symbol;
+	job(argv, environ);
+	return (0);
+} */
+
+int	build_tree(t_node **root, t_token *token)
+{
+	(void)root;
+	(void)token;
+	return (0);
+}
+
 int	parser(struct s_queue *queue)
 {
+	t_node	*tree;
 	t_token	*token;
-	char	**argv;
-
+/*	char	**argv;
+*/
 	queue_apply_to_each(queue, display_tokens); /* Verify tokens */
 
 	/* This part should not be in parser, just for test during the building */
-	extern char **environ;
-	while ((token = queue_dequeue(queue, NULL))) /* not right free function for tokens */
+/*	extern char **environ;
+*/	while ((token = queue_dequeue(queue, NULL))) /* not right free function for tokens */
 	{
-		argv = (char**)ft_memalloc(sizeof(char*) * 2);
-		*argv = token->symbol;
-		job(argv, environ);
+		build_tree(&tree, token);
 	}
 	/* End of test part */
 	return (e_success);
