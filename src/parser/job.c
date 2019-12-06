26ec86fb55b23608a9e18b2afb5eb81a1aa089c1
Antoine Barthelemy <abarthel@student.42.fr>
2019-12-06 16:43:16 +0100

    feat(job control): get status information on jobs

--- src/parser/job.c
+++ src/parser/job.c
@@ -252,3 +252,27 @@ void	do_job_notification(void)
 			jlast = j;
 	}
 }
+
+
+/* Shell internal jobs bookkeeping
+*/
+
+/* Mark a stopped job J as being running again. */
+void	mark_job_as_running(t_job *j)
+{
+	t_process	*p;
+
+	for (p = j->first_process; p; p = p->next)
+		p->stopped = 0;
+	j->notified = 0;
+}
+
+/* Continue the job J. */
+void	continue_job(t_job *j, int foreground)
+{
+	mark_job_as_running(j);
+	if (foreground)
+		put_job_in_foreground(j, 1);
+	else
+		put_job_in_background(j, 1);
+}
