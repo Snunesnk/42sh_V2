0b8dcc28c25ec294fc30d06f1e8dfa016610c0f9
Antoine Barthelemy <abarthel@student.42.fr>
2019-12-09 18:23:46 +0100

    feat(job control): adding job launch and pipe mounting

--- src/parser/execute_line.c
+++ src/parser/execute_line.c
@@ -4,6 +4,15 @@
 #include "libft.h"
 #include "job_control.h"
 
+void	print_p(t_process *p)
+{
+	while (p)
+	{
+		ft_print_tables(p->argv);
+		p = p->next;
+	}
+}
+
 void	ft_print_tables(char **tables)
 {
 	size_t	i;
@@ -86,45 +95,72 @@ t_process	*build_a_process(t_list **lst)
 	return (NULL);
 }
 
+t_process	*build_processes(t_list **lst)
+{
+	t_process	*first_process;
+	t_process	*p;
+	t_token		*t;
+
+
+	first_process = build_a_process(lst);
+	p = first_process;
+	while (*lst)
+	{
+		while (*lst && (t = (*lst)->content) && t->type != WORD)
+			(*lst) = (*lst)->next;
+		p = p->next;
+		p = build_a_process(lst);
+		if (p == NULL)
+			return (NULL);
+/*		printf("--- next job ---\n");  //Debug
+		ft_print_tables(p->argv);      //Debug
+*/	}
+	return (first_process);
+}
+
 t_job	*build_a_job(t_list **lst)
 {
 	t_job	*j;
 	t_token	*t;
 
-	j = (t_job*)ft_memalloc(sizeof(t_job));
+	j = NULL;
 	while (*lst)
 	{
-		j->first_process = build_a_process(lst);
-		ft_print_tables(j->first_process->argv);
-		printf("--- next job ---\n");
+		j = (t_job*)ft_memalloc(sizeof(t_job));
+		if (j == NULL)
+			return (NULL);
+		j->first_process = build_processes(lst);
+		print_p(j->first_process); /* debug */
 		while (*lst && (t = (*lst)->content) && t->type != WORD)
 			(*lst) = (*lst)->next;
 	}
-	exit(21);
-	return (NULL);
+	return (j);
 
+}
+
+t_job	*build_jobs(t_list **lst)
+{
+	return (build_a_job(lst));
 }
 
 int	execute_line(t_list *lst)
 {
-	t_job	*newjob;
-	t_job	*j;
+	t_job		*j;
+	t_process	*p;
 
 	lst = lst->next;
-	j = NULL;
-	while (lst)
+	j = build_jobs(&lst);
+	while (j)
 	{
-		if ((newjob = build_a_job(&lst)) != NULL)
+		p = j->first_process;
+		while (p)
 		{
-			if (j == NULL)
-				j = newjob;
-			else
-				j->next = newjob;
+			ft_print_tables(p->argv);
+			p = p->next;
 		}
-		else
-			exit(21);
+		j = j->next;
 	}
-
+	exit(21);
 /*	launch_job(t_job *j, int foreground)
 */
 /*	launch_job(j, 1);
