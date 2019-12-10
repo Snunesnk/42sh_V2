5a10fdf57f373cfab5574bf5056843b82e06fc99
Antoine Barthelemy <abarthel@student.42.fr>
2019-12-10 15:42:23 +0100

    feat(job control): going through execution tests

--- src/parser/execute_line.c
+++ src/parser/execute_line.c
@@ -4,6 +4,7 @@
 #include "libft.h"
 #include "job_control.h"
 
+
 void	print_p(t_process *p)
 {
 	while (p)
@@ -55,92 +56,112 @@ char		*get_tokvalue(t_list *lst)
 	return (t->value);
 }
 
-char		**build_argv(t_list **lst)
+int		build_argv(char ***argv, t_list **lst)
 {
 	t_token	*t;
-	char	**argv;
 	int	argc;
 	int	i;
 
 	i = 0;
+//	printf("step D\n");
 	argc = get_argc(*lst);
-	argv = (char**)ft_memalloc(sizeof(char*) * (argc + 1));
-	if (argv != NULL)
+//	printf("step E\n");
+	*argv = (char**)ft_memalloc(sizeof(char*) * (argc + 1));
+	if (*argv != NULL)
 	{
+//		printf("step F\n");
 		while (*lst && i < argc)
 		{
-			argv[i] = get_tokvalue(*lst);
+			(*argv)[i] = get_tokvalue(*lst);
 			++i;
 			(*lst) = (*lst)->next;
 		}
+//		printf("Succes step F\n");
+		return (SUCCESS);
 	}
-	return (argv);
+	printf("Failed step F\n");
+	return (FAILURE);
 }
 
-t_process	*build_a_process(t_list **lst)
+int	build_a_process(t_process **p, t_list **lst)
 {
-	t_process	*p;
 	t_token		*t;
 	char		**argv;
-
-	p = (t_process*)ft_memalloc(sizeof(t_process));
+/*	static int	i;
+*/
+//	printf("step C\n");
+	*p = (t_process*)ft_memalloc(sizeof(t_process));
 	while (*lst)
 	{
-		p->argv = build_argv(lst);
-		if (p->argv == NULL)
-			return (NULL);
+//		printf("step C\n");
+		if (build_argv(&((*p)->argv), lst) == FAILURE)
+		{
+//			printf("Failed step G\n");
+			return (FAILURE);
+		}
 		else
-			return (p);
+		{
+/*			printf("step G\n");
+			printf("-> %d\t" , ++i);
+			ft_print_tables((*p)->argv);
+*/			return (SUCCESS);
+		}
 	}
-	return (NULL);
+	return (FAILURE);
 }
 
-t_process	*build_processes(t_list **lst)
+int	build_processes(t_process **first_process, t_list **lst)
 {
-	t_process	*first_process;
 	t_process	*p;
 	t_token		*t;
 
-
-	first_process = build_a_process(lst);
-	p = first_process;
+//	printf("step B\n");
+	if (build_a_process(first_process, lst) == FAILURE)
+	{
+		return (FAILURE);
+	}
+//	printf("step H\n");
+	p = *first_process;
 	while (*lst)
 	{
 		while (*lst && (t = (*lst)->content) && t->type != WORD)
 			(*lst) = (*lst)->next;
+		if (*lst == NULL)
+			break;
+		if (build_a_process(&(p->next), lst) == FAILURE)
+			return (FAILURE);
 		p = p->next;
-		p = build_a_process(lst);
-		if (p == NULL)
-			return (NULL);
 /*		printf("--- next job ---\n");  //Debug
 		ft_print_tables(p->argv);      //Debug
 */	}
-	return (first_process);
+/*	printf("ALL:\n");
+	print_p(first_process);
+*/	return (SUCCESS);
 }
 
-t_job	*build_a_job(t_list **lst)
+int	build_a_job(t_job **j, t_list **lst)
 {
-	t_job	*j;
 	t_token	*t;
 
-	j = NULL;
-	while (*lst)
+/*	while (*lst)
 	{
-		j = (t_job*)ft_memalloc(sizeof(t_job));
-		if (j == NULL)
-			return (NULL);
-		j->first_process = build_processes(lst);
-		print_p(j->first_process); /* debug */
-		while (*lst && (t = (*lst)->content) && t->type != WORD)
+*/		*j = (t_job*)ft_memalloc(sizeof(t_job));
+		if (*j == NULL)
+			return (FAILURE); /* error malloc */
+/*		printf("step A\n");
+*/		if (build_processes(&((*j)->first_process), lst) == FAILURE)
+			return (FAILURE);
+/*		print_p((*j)->first_process); // debug 
+*//*		while (*lst && (t = (*lst)->content) && t->type != WORD)
 			(*lst) = (*lst)->next;
 	}
-	return (j);
+*/	return (SUCCESS);
 
 }
 
-t_job	*build_jobs(t_list **lst)
+int	build_jobs(t_job **j, t_list **lst)
 {
-	return (build_a_job(lst));
+	return (build_a_job(j, lst));
 }
 
 int	execute_line(t_list *lst)
@@ -149,7 +170,9 @@ int	execute_line(t_list *lst)
 	t_process	*p;
 
 	lst = lst->next;
-	j = build_jobs(&lst);
+	j = NULL;
+	if (build_jobs(&j, &lst) == FAILURE)
+		return (FAILURE);
 	while (j)
 	{
 		p = j->first_process;
@@ -160,7 +183,6 @@ int	execute_line(t_list *lst)
 		}
 		j = j->next;
 	}
-	exit(21);
 /*	launch_job(t_job *j, int foreground)
 */
 /*	launch_job(j, 1);
