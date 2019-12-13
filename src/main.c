4f9c5fc1dd2329e586629b274744a77460f25a99
Antoine Barthelemy <abarthel@student.42.fr>
2019-12-13 16:07:40 +0100

    new arch

--- src/main.c
+++ src/main.c
@@ -11,7 +11,25 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "21sh.h"
+#include "shell.h"
+
+static char	*ft_join_free(char *s1, char *s2, int op)
+{
+	char	*str;
+
+	str = ft_strjoin(s1, s2);
+	if (op == 1)
+		ft_strdel(&s1);
+	else if (op == 2)
+		ft_strdel(&s2);
+	else
+	{
+		ft_strdel(&s1);
+		ft_strdel(&s2);
+	}
+	return (str);
+}
+
 
 static void	del(void *content, size_t content_size)
 {
--- src/main.d
+++
@@ -1,5 +0,0 @@
-src/main.o: src/main.c libft/include/libft.h src/21sh.h
-
-libft/include/libft.h:
-
-src/21sh.h:
