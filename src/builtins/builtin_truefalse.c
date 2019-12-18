0999e3fd407c9f514e765b8d69587e823e0566da
Antoine Barthelemy <abarthel@student.42.fr>
2019-12-18 09:59:14 +0100

    fix (env builtins): fix call to the right ft_print_tables

--- src/builtins/builtin_truefalse.c
+++ src/builtins/builtin_truefalse.c
@@ -6,9 +6,12 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 20:52:32 by abarthel          #+#    #+#             */
-/*   Updated: 2019/12/18 09:50:32 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/12/18 09:52:47 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "job_control.h"
+#include "builtins.h"
 
 int	cmd_true(int argc, t_process *p)
 {
