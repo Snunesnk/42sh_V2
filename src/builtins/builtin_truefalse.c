197e688ba250d118e6106da9f26bf12877748cf1
Antoine Barthelemy <abarthel@student.42.fr>
2019-12-18 09:50:51 +0100

    fix (true and false builtins): fix with new prototype containing t_process

--- src/builtins/builtin_truefalse.c
+++ src/builtins/builtin_truefalse.c
@@ -6,20 +6,20 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 20:52:32 by abarthel          #+#    #+#             */
-/*   Updated: 2019/07/21 19:13:09 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/12/18 09:50:32 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	cmd_true(int argc, char **argv)
+int	cmd_true(int argc, t_process *p)
 {
 	(void)argc;
-	(void)argv;
+	(void)p;
 	return (1);
 }
 
-int	cmd_false(int argc, char **argv)
+int	cmd_false(int argc, t_process *p)
 {
 	(void)argc;
-	(void)argv;
+	(void)p;
 	return (0);
 }
