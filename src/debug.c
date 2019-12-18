fc93713b372aef1ea13466abc115ddefd090bd63
Simon Nunes <nunesimon85@gmail.com>
2019-12-18 14:58:03 +0100

    add token IO_NB

--- src/debug.c
+++ src/debug.c
@@ -6,7 +6,7 @@
 /*   By: efischer <efischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/02 14:31:26 by efischer          #+#    #+#             */
-/*   Updated: 2019/12/18 13:22:28 by efischer         ###   ########.fr       */
+/*   Updated: 2019/12/18 14:57:09 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ static void	init_token_tab(char **token_tab)
 	token_tab[GREAT] = "GREAT";
 	token_tab[LESS] = "LESS";
 	token_tab[WORD] = "WORD";
+	token_tab[IO_NB] = "IO_NB";
 	token_tab[COMMENT] = "COMMENT";
 	token_tab[START] = "START";
 	token_tab[END] = "END";
@@ -50,7 +51,7 @@ static void	print(t_list *lst, t_list **elem)
 		tmp = NULL;
 		if (((t_token*)(lst->content))->type == i)
 		{
-			if (i == WORD)
+			if (i == WORD || i == IO_NB)
 				tmp = ((t_token*)(lst->content))->value->str;
 			ft_asprintf(&print_content, "%s -> [%s]\n", token_tab[i], tmp);
 			break ;
