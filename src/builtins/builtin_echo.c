9aa9753e2cb057a384f1f84c3f951f5e7a5e00fa
Simon Nunes <nunesimon85@gmail.com>
2019-12-18 16:14:40 +0100

    clean headers

--- src/builtins/builtin_echo.c
+++ src/builtins/builtin_echo.c
@@ -6,13 +6,11 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 20:52:32 by abarthel          #+#    #+#             */
-/*   Updated: 2019/07/21 19:13:09 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/12/18 15:53:50 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include "shell.h"
-#include "job_control.h"
 
 int	cmd_echo(int argc, t_process *p)
 {
