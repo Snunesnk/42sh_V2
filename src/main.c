ed55893e8d9208829dfb08e9040989a9ac2ca669
Antoine Barthelemy <abarthel@student.42.fr>
2020-01-03 15:49:27 +0100

    archi(job control): restructuring for simplier architecture and make it easier to understand building of jobs and their execution

--- src/main.c
+++ src/main.c
@@ -19,48 +19,7 @@
 #include "shell.h"
 #include "error.h"
 
-int		g_retval;
-char		g_pwd[] = {0};
 extern char	**environ;
-
-static int	set_minimal_env(void)
-{
-	char	*tmp;
-	int		shlvl;
-
-	tmp = getcwd(NULL, 0);
-	if (ft_setenv("PWD", tmp, 1))
-		return (e_cannot_allocate_memory);
-	ft_memdel((void**)&tmp);
-	if (!(tmp = ft_getenv("SHLVL")))
-	{
-		if (ft_setenv("SHLVL", "1", 1))
-			return (e_cannot_allocate_memory);
-	}
-	else
-	{
-		shlvl = ft_atoi(tmp) + 1;
-		tmp = ft_itoa(shlvl);
-		if (ft_setenv("SHLVL", tmp, 1))
-			return (e_cannot_allocate_memory);
-		ft_memdel((void**)&tmp);
-	}
-	if (PATH_MAX > 0)
-	{
-		tmp = ft_getenv("PWD");
-		if (ft_strlen(tmp) <= PATH_MAX)
-			ft_strcpy(g_pwd, ft_getenv("PWD"));
-	}
-	return (e_success);
-}
-
-static void	del(void *content, size_t content_size)
-{
-	(void)content_size;
-	vct_del(((t_token*)(content))->value);
-	free(content);
-}
-
 
 int			main(int argc, char **argv)
 {
