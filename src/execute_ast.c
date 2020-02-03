548583bdc82d8ef60c173c67871ffe744041ad5e
Antoine Barthelemy <abarthel@student.42.fr>
2020-02-04 00:52:11 +0100

    fix(comment): pid of background shell

--- src/execute_ast.c
+++ src/execute_ast.c
@@ -17,7 +17,7 @@ int	execute_and(t_ast *node)
 {
 	/* launch a subshell with node->left */
 	if (node->left)
-		execute_subshell(node->left); /* Get the PID and add it to the list ????  not sure */
+		execute_subshell(node->left); /* Get the PID and add it to the list in case fg and ctrl + c */
 	if (node->right)
 		return (execute_node(node->right));
 }
