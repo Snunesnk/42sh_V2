f7730c7ee7009814a4e8dffb867af5ad1b5d268d
Simon Nunes <nunesimon85@gmail.com>
2020-02-12 10:48:13 +0100

    new file src/ast.c

---
+++ src/ast.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ast.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efischer <efischer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/12 10:39:25 by efischer          #+#    #+#             */
+/*   Updated: 2020/02/12 10:45:12 by efischer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "shell.h"
+
+static void		astadd_left(t_ast **ast, t_ast *new_ast)
+{
+	t_ast	*head;
+
+	head = *ast;
+	if (*ast == NULL)
+		*ast = new_ast;
+	else
+	{
+		while ((*ast)->left != NULL)
+			*ast = (*ast)->right;
+		(*ast)->left = new_ast;
+		*ast = head;
+	}
+}
+
+static void		astadd_right(t_ast **ast, t_ast *new_ast)
+{
+	t_ast	*head;
+
+	head = *ast;
+	if (*ast == NULL)
+		*ast = new_ast;
+	else
+	{
+		while ((*ast)->right != NULL)
+			*ast = (*ast)->right;
+		(*ast)->right = new_ast;
+		*ast = head;
+	}
+}
+
+static t_ast	*astnew(t_list *lst, uint64_t type)
+{
+	t_ast	*ast;
+
+	ast = (t_ast*)malloc(sizeof(t_ast));
+	if (ast != NULL)
+	{
+		ft_bzero(ast, sizeof(t_ast));
+		ast->type = type;
+		ast->content = lst;
+	}
+	return (ast);
+}
+
+void		build_ast(uint64_t type, t_ast **ast, t_list *lst)
+{
+	t_ast		*new_ast;
+
+	if (type == AND || type == AND_IF || type == OR_IF || type == SEMI)
+	{
+		new_ast = astnew(NULL, type);
+		astadd_right(ast, new_ast);
+		new_ast = astnew(lst, NONE);
+		astadd_left(ast, new_ast);
+	}
+	else
+	{
+		new_ast = astnew(lst, NONE);
+		astadd_right(ast, new_ast);
+	}
+}
