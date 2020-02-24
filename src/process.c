fd59e75ec5c387dc235a2ddca0241f5bbb9fb7e8
Antoine Barthelemy <abarthel@student.42.fr>
2020-02-24 23:44:20 +0100

    fix(job control): go coding proper functions of job control

--- src/process.c
+++ src/process.c
@@ -32,12 +32,8 @@ int	launch_process(t_process *p, pid_t pgid, int infile, int outfile, int errfil
 			pgid = pid;
 		}
 		setpgid(pid, pgid);
-		(void)foreground; /* WTF, why commenting this avoid crashes ????? */
-//		if (foreground)
-//		{
-	//		(void)foreground;
-//			tcsetpgrp(shell_terminal, pgid);
-//		}
+		if (foreground)
+			tcsetpgrp(shell_terminal, pgid);
 		/* Set the handling for job control signals back to the default. */
 		restore_procmask();
 	}
