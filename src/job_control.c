e6d80f1b8109886f887cead6ca407973b26a7539
Antoine Barthelemy <abarthel@student.42.fr>
2020-02-24 18:43:48 +0100

    fix(subshell): adding subshell mgt

--- src/job_control.c
+++ src/job_control.c
@@ -38,7 +38,6 @@ void	put_job_in_foreground(t_job *j, int cont)
 
 /* Put a job in the background. If the cont argument is true, send
    the process group a SIGCONT signal to wake it up. */
-
 void	put_job_in_background(t_job *j, int cont)
 {
 	if (cont) /* Send the job a continue signal, if necessary.*/
