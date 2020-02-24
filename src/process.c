76f1cb33008b374d2cf808a8d2647f0c97e225c9
Antoine Barthelemy <abarthel@student.42.fr>
2020-02-24 15:24:28 +0100

    fix(job control): implementation capturing correctly pid and using reliable signals with procmask

--- src/process.c
+++ src/process.c
@@ -37,12 +37,7 @@ int	launch_process(t_process *p, pid_t pgid, int infile, int outfile, int errfil
 			tcsetpgrp(shell_terminal, pgid);
 		}
 		/* Set the handling for job control signals back to the default. */
-		signal(SIGINT, SIG_DFL);
-		signal(SIGQUIT, SIG_DFL);
-		signal(SIGTSTP, SIG_DFL);
-		signal(SIGTTIN, SIG_DFL);
-		signal(SIGTTOU, SIG_DFL);
-		signal(SIGCHLD, SIG_DFL);
+		restore_procmask();
 	}
 	/* Set the standard input/output channels of the new process. */
 
