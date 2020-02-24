8e054b7ee0d18614830c87d4c3a7e9ba769e2f44
Antoine Barthelemy <abarthel@student.42.fr>
2020-02-24 20:06:37 +0100

    fix(subshell): adding subshell for background...

--- src/signals.c
+++ src/signals.c
@@ -2,6 +2,7 @@
 
 sigset_t		g_save_procmask;
 struct sigaction	g_shell_sset;
+struct sigaction	g_sigchld_mask;
 
 void	handle_chld(int signo, siginfo_t *info, void *context)
 {
@@ -9,6 +10,27 @@ void	handle_chld(int signo, siginfo_t *info, void *context)
 	(void)context;
 	ft_printf("\nsending pid:%d\n", info->si_pid);
 	update_status();
+}
+
+/* Update could be done by catching signals instead of update_status before prompt */
+void	set_sigchild_handler(void)
+{
+	/* SIGCHLD set */
+	sigemptyset(&g_shell_sset.sa_mask);
+	sigaddset(&g_shell_sset.sa_mask, SIGINT);
+	sigaddset(&g_shell_sset.sa_mask, SIGQUIT);
+	sigaddset(&g_shell_sset.sa_mask, SIGTSTP);
+	sigaddset(&g_shell_sset.sa_mask, SIGTTIN);
+	sigaddset(&g_shell_sset.sa_mask, SIGTTOU);
+	sigaddset(&g_shell_sset.sa_mask, SIGCHLD);
+	g_shell_sset.sa_flags = SA_SIGINFO | SA_RESTART;
+	g_shell_sset.sa_sigaction = handle_chld; /* Update child status handler, function */
+	sigaction(SIGCHLD, &g_shell_sset, NULL);
+}
+
+void	reset_sigchild_handler(void)
+{
+	sigaction(SIGCHLD, &g_shell_sset, NULL);
 }
 
 /* Block terminal generated signals, save old mask and set shell one */
@@ -23,19 +45,9 @@ void	init_shell_sset(void)
 	sigaddset(&newmask, SIGTSTP);
 	sigaddset(&newmask, SIGTTIN);
 	sigaddset(&newmask, SIGTTOU);
+	sigaddset(&newmask, SIGCHLD); /* Test */
 	if (sigprocmask(SIG_BLOCK, &newmask, &g_save_procmask))
 		exit(1);
-	/* SIGCHLD set */
-//	sigemptyset(&g_shell_sset.sa_mask);
-//	sigaddset(&g_shell_sset.sa_mask, SIGINT);
-//	sigaddset(&g_shell_sset.sa_mask, SIGQUIT);
-//	sigaddset(&g_shell_sset.sa_mask, SIGTSTP);
-//	sigaddset(&g_shell_sset.sa_mask, SIGTTIN);
-//	sigaddset(&g_shell_sset.sa_mask, SIGTTOU);
-//	sigaddset(&g_shell_sset.sa_mask, SIGCHLD);
-//	g_shell_sset.sa_flags = SA_SIGINFO | SA_RESTART;
-//	g_shell_sset.sa_sigaction = handle_chld; /* Update child status handler, function */
-//	sigaction(SIGCHLD, &g_shell_sset, NULL);
 }
 
 /* Restore proc mask inheritated from shell starting proc*/
