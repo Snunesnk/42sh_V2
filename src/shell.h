a210fcd01845c595d5e38c35bb12499e3465e58b
Antoine Barthelemy <abarthel@student.42.fr>
2020-02-25 11:40:43 +0100

    fix(norm): shell.h

--- src/shell.h
+++ src/shell.h
@@ -84,28 +84,28 @@ struct	s_redirection
 /* A process is a single process.  */
 struct s_process
 {
-  struct s_process	*next;       /* next process in pipeline */
-  char			**argv;      /* for exec */
-  pid_t			pid;         /* process ID, given at fork time in job.c,
-						uses waitpid (waitpid is not called when & at end of job) */
-  char			completed;   /* true if process has completed */
-  char			stopped;     /* true if process has stopped */
-  int			status;      /* reported status value */
-  struct s_redirection	*redir;      /* all recirections to be applied */
+	struct s_process	*next;       /* next process in pipeline */
+	char			**argv;      /* for exec */
+	pid_t			pid;         /* process ID, given at fork time in job.c,
+	      					uses waitpid (waitpid is not called when & at end of job) */
+	char			completed;   /* true if process has completed */
+	char			stopped;     /* true if process has stopped */
+	int			status;      /* reported status value */
+	struct s_redirection	*redir;      /* all recirections to be applied */
 };
 
 /* A job is a pipeline of processes.  */
 struct s_job
 {
-  struct s_job		*next;          /* next active job */
-  char			*command;       /* command line, used for messages */
-  struct s_process	*first_process; /* list of processes in this job */
-  pid_t			pgid;           /* process group ID */
-  char			notified;       /* true if user told about stopped job */
-  struct termios	tmodes;         /* saved terminal modes */
-  int			stdin;          /* standard i/o channels */
-  int			stdout;         /* standard i/o channels */
-  int			stderr;         /* standard i/o channels */
+	struct s_job		*next;          /* next active job */
+	char			*command;       /* command line, used for messages */
+	struct s_process	*first_process; /* list of processes in this job */
+	pid_t			pgid;           /* process group ID */
+	char			notified;       /* true if user told about stopped job */
+	struct termios	tmodes;         /* saved terminal modes */
+	int			stdin;          /* standard i/o channels */
+	int			stdout;         /* standard i/o channels */
+	int			stderr;         /* standard i/o channels */
 };
 
 typedef struct s_process	t_process;
