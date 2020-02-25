8041530a5c99750b9eaa26c327e90b6d9b60688b
Antoine Barthelemy <abarthel@student.42.fr>
2020-02-25 17:51:48 +0100

    feat(builtins): adding bg, lazy to overkill it, this project will beat me...

--- src/builtins/builtin_bg.c
+++ src/builtins/builtin_bg.c
@@ -12,9 +12,26 @@
 
 #include "shell.h"
 
+/*
+**  https://pubs.opengroup.org/onlinepubs/9699919799/utilities/fg.html
+*/
+
 int	cmd_bg(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	t_job	*j;
+
+	if (argc == 2)
+	{
+		j = find_job(ft_atoi(argv[1]));
+		put_job_in_background(j, 1);
+	}
+//	else if (argc > 2)
+//	{
+//		if (argv[1][0] == '-')
+//			ft_dprintf(STDERR_FILENO, "fg: -l: invalid option\n");
+	else
+		ft_dprintf(STDERR_FILENO, "bg: usage: bg [job_spec]\n");
+//	}
+		
 	return (0);
 }
