687ed534f95abdfbd9fff7a47925e762c4775eab
Antoine Barthelemy <abarthel@student.42.fr>
2020-02-28 22:58:50 +0100

    fix(redir): redir fixes on the go

--- src/undo_redir.c
+++ src/undo_redir.c
@@ -19,11 +19,49 @@ static void	debug_r(t_redirection *r)
 //	ft_printf("\n");
 }
 
-static int	undo_iowrite(t_redirection *r, int *shell_fd)
+static int	restored_fd(t_shell_fds *shell_fd, int fd)
 {
-	if (r->redirector.dest < 3 && !shell_fd[r->redirector.dest])
+	while (shell_fd)
 	{
-		shell_fd[r->redirector.dest] = 1;
+		if (shell_fd->fd == fd)
+			return (1);
+		shell_fd = shell_fd->next;
+	}
+	return (0);
+}
+
+static void	add_restored_fd(t_shell_fds **shell_fd, int add)
+{
+	t_shell_fds	*s;
+
+	if (!*shell_fd)
+	{
+		*shell_fd = ft_memalloc(sizeof(t_shell_fds));
+		(*shell_fd)->fd = add;
+	}
+	else
+	{
+		s = *shell_fd;
+		while (s->next)
+			s = s->next;
+		s->next = ft_memalloc(sizeof(t_shell_fds));
+		s->next->fd = add;
+	}
+}
+
+static void	free_restored_fd(t_shell_fds *l)
+{
+	if (l)
+	{
+		free_restored_fd(l->next);
+		free(l);
+	}
+}
+
+static int	undo_iowrite(t_redirection *r, t_shell_fds **shell_fd)
+{
+	if (!restored_fd(*shell_fd, r->redirector.dest))
+	{
 		dup2(r->save, r->redirector.dest);
 	}
 	else if (r->redirector.dest >= 3 && shell_fd[r->redirector.dest])
@@ -32,46 +70,45 @@ static int	undo_iowrite(t_redirection *r, int *shell_fd)
 	return (0);
 }
 
-static int	undo_ioread(t_redirection *r, int *shell_fd)
+static int	undo_ioread(t_redirection *r, t_shell_fds **shell_fd)
 {
 	debug_r(r); /* Debug */
 	/* Multiple close of STDIN are done */
 	/* HERE IS THE HUGE BUG CLOSING STDIN multiple times, need fcntl to check ?*/
-	if (r->redirectee.dest < 3 && !shell_fd[r->redirectee.dest])
+//	if (r->redirectee.dest < 3 && !shell_fd[r->redirectee.dest])
+	if (!restored_fd(*shell_fd, r->redirectee.dest))
 	{
-		shell_fd[r->redirectee.dest] = 1;
+		add_restored_fd(shell_fd, r->redirectee.dest);
 		dup2(r->save, r->redirectee.dest);
 	}
-	else if (r->redirectee.dest >= 3 && shell_fd[r->redirectee.dest])
-		dup2(r->save, r->redirectee.dest);
 	close(r->save);
 	return (0);
 }
 
 int	undo_redirection_internal(t_redirection *r)
 {
-	int	shell_fd[3];
+	t_shell_fds	*shell_fd;
 
-	shell_fd[0] = 0;
-	shell_fd[1] = 0;
-	shell_fd[2] = 0;
+	shell_fd = NULL;
 	while (r)
 	{
 		if (r->flags & REDSUC)
 		{
 			if (r->instruction == IOWRITE)
-				undo_iowrite(r, shell_fd);
+				undo_iowrite(r, &shell_fd);
 			else if (r->instruction == IOCAT)
-				undo_iowrite(r, shell_fd);
+				undo_iowrite(r, &shell_fd);
 			else if (r->instruction == IOREAD)
-				undo_ioread(r, shell_fd);
+				undo_ioread(r, &shell_fd);
 			else if (r->instruction == IODUP && !(r->flags & FDCLOSE))
-				undo_iowrite(r, shell_fd);
+				undo_iowrite(r, &shell_fd);
 			else if (r->instruction == (IODUP | IOREAD) && !(r->flags & FDCLOSE))
-				undo_ioread(r, shell_fd);
+				undo_ioread(r, &shell_fd);
 		}
 		r = r->next;
 	}
+	/* free shell_fd list here */
+	free_restored_fd(shell_fd);
 	return (0);
 }
 
@@ -80,8 +117,7 @@ int	undo_redirection(t_redirection *r)
 	if (r)
 	{
 		undo_redirection_internal(r);
-		/* should we free at that point ? */
-		r = r->next;
+		/* should we free at that point ? think so */
 	}
 	return (0);
 }
