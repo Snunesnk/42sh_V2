4c1d5a261b87b6443e9b0e930ff5fbac6894fede
Antoine Barthelemy <abarthel@student.42.fr>
2020-02-28 17:10:41 +0100

    fix(redir): fixed multiple close of shell fds, still need tests on other redirection types and fds and single builtin

--- src/undo_redir.c
+++ src/undo_redir.c
@@ -19,40 +19,56 @@ static void	debug_r(t_redirection *r)
 //	ft_printf("\n");
 }
 
-
-static int	undo_iowrite(t_redirection *r)
+static int	undo_iowrite(t_redirection *r, int *shell_fd)
 {
-	dup2(r->save, r->redirector.dest);
+	if (r->redirector.dest < 3 && !shell_fd[r->redirector.dest])
+	{
+		shell_fd[r->redirector.dest] = 1;
+		dup2(r->save, r->redirector.dest);
+	}
+	else if (r->redirector.dest >= 3 && shell_fd[r->redirector.dest])
+		dup2(r->save, r->redirector.dest);
 	close(r->save);
 	return (0);
 }
 
-static int	undo_ioread(t_redirection *r)
+static int	undo_ioread(t_redirection *r, int *shell_fd)
 {
 	debug_r(r); /* Debug */
 	/* Multiple close of STDIN are done */
 	/* HERE IS THE HUGE BUG CLOSING STDIN multiple times, need fcntl to check ?*/
-	dup2(r->save, r->redirectee.dest);
+	if (r->redirectee.dest < 3 && !shell_fd[r->redirectee.dest])
+	{
+		shell_fd[r->redirectee.dest] = 1;
+		dup2(r->save, r->redirectee.dest);
+	}
+	else if (r->redirectee.dest >= 3 && shell_fd[r->redirectee.dest])
+		dup2(r->save, r->redirectee.dest);
 	close(r->save);
 	return (0);
 }
 
 int	undo_redirection_internal(t_redirection *r)
 {
+	int	shell_fd[3];
+
+	shell_fd[0] = 0;
+	shell_fd[1] = 0;
+	shell_fd[2] = 0;
 	while (r)
 	{
 		if (r->flags & REDSUC)
 		{
 			if (r->instruction == IOWRITE)
-				undo_iowrite(r);
+				undo_iowrite(r, shell_fd);
 			else if (r->instruction == IOCAT)
-				undo_iowrite(r);
+				undo_iowrite(r, shell_fd);
 			else if (r->instruction == IOREAD)
-				undo_ioread(r);
+				undo_ioread(r, shell_fd);
 			else if (r->instruction == IODUP && !(r->flags & FDCLOSE))
-				undo_iowrite(r);
+				undo_iowrite(r, shell_fd);
 			else if (r->instruction == (IODUP | IOREAD) && !(r->flags & FDCLOSE))
-				undo_ioread(r);
+				undo_ioread(r, shell_fd);
 		}
 		r = r->next;
 	}
