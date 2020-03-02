b9e0be4f0487e957cb42e416af2dbea5c5e707f0
Antoine Barthelemy <abarthel@student.42.fr>
2020-03-02 14:25:51 +0100

    fix(redir): andgreat fixed

--- src/undo_redir.c
+++ src/undo_redir.c
@@ -77,6 +77,27 @@ static int	undo_iodup(t_redirection *r, t_shell_fds **shell_fd)
 	return (0);
 }
 
+static int	undo_iodfile(t_redirection *r, t_shell_fds **shell_fd)
+{
+	if (r->redirectee.dest == r->redirector.dest)
+		return (0);
+	close(r->redirectee.dest);
+	if (!restored_fd(*shell_fd, STDOUT_FILENO))
+	{
+		add_restored_fd(shell_fd, STDOUT_FILENO);
+		dup2(r->save[0], STDOUT_FILENO);
+	}
+	close(r->save[0]);
+	if (!restored_fd(*shell_fd, STDERR_FILENO))
+	{
+		add_restored_fd(shell_fd, STDERR_FILENO);
+		dup2(r->save[1], STDERR_FILENO);
+	}
+	close(r->save[1]);
+	return (0);
+}
+
+
 int	undo_redirection_internal(t_redirection *r)
 {
 	t_shell_fds	*shell_fd;
@@ -92,6 +113,8 @@ int	undo_redirection_internal(t_redirection *r)
 				undo_iowrite(r, &shell_fd);
 			else if (r->instruction == IOREAD)
 				undo_ioread(r, &shell_fd);
+			else if (r->instruction == (IODUP | IOWRITE))
+				undo_iodfile(r, &shell_fd);
 			else if (r->instruction == IODUP && !(r->flags & FDCLOSE))
 				undo_iodup(r, &shell_fd);
 			else if (r->instruction == (IODUP | IOREAD) && !(r->flags & FDCLOSE))
