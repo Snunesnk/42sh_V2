fc90d35f95b95601db0ea719d884cc1ce08d3561
Simon Nunes <nunesimon85@gmail.com>
2020-03-05 08:09:16 +0100

    src/builtins/builtin_unset.c: unset del bug fixed

--- src/builtins/builtin_unset.c
+++ src/builtins/builtin_unset.c
@@ -1,6 +1,22 @@
 #include "libft.h"
 #include "shell.h"
 
+static void	ft_free_tab(char **tmp)
+{
+	int		i;
+
+	i = 0;
+	if (!tmp)
+		return ;
+	while (tmp[i])
+	{
+		free(tmp[i]);
+		tmp[i++] = NULL;
+	}
+	free(tmp);
+	tmp = NULL;
+}
+
 static void	del_elem(void *content, size_t content_size)
 {
 	(void)content_size;
@@ -32,17 +48,63 @@ static char	**get_name_tab(int ac, char **av)
 	return (tab);
 }
 
-int			cmd_unset(int ac, char **av)
+static int	match_name(char *var_name, char **name)
+{
+	int		ret;
+	size_t	i;
+
+	i = 0;
+	ret = FALSE;
+	while (name[i] != NULL)
+	{
+		if (ft_strequ(var_name, name[i]) == TRUE)
+		{
+			ret = TRUE;
+			break ;
+		}
+		i++;
+	}
+	return (ret);
+}
+
+static void	unset_var(char **name)
 {
 	extern t_list	*g_env;
 	t_list			*head;
-	t_list			
-	int				ret;
-	char			**name;
-	size_t			i;
+	t_list			*tmp;
+
+	head = g_env;
+	while (g_env != NULL && match_name(((t_shell_var*)(g_env->content))->name, name) == TRUE)
+	{
+		head = g_env->next;
+		ft_lstdelone(&g_env, &del_elem);
+		g_env = head;
+	}
+	tmp = g_env;
+	g_env = g_env->next;
+	while (g_env != NULL)
+	{
+		if (match_name(((t_shell_var*)(g_env->content))->name, name) == TRUE)
+		{
+			tmp->next = g_env->next;
+			ft_lstdelone(&g_env, &del_elem);
+			g_env = tmp->next;
+		}
+		else
+		{
+			tmp = g_env;
+			g_env = g_env->next;
+		}
+	}
+	g_env = head;
+}
+
+int			cmd_unset(int ac, char **av)
+{
+	int		ret;
+	char	**name;
 
 	ret = SUCCESS;
-	head = g_env;
 	if (ft_getopt(ac - 1, av, "") == FALSE)
 	{
 		ft_putendl_fd("unset: usage: unset [arg ...]", 2);
@@ -50,25 +112,7 @@ int			cmd_unset(int ac, char **av)
 	}
 	name = get_name_tab(ac - 1, av + 1);
 	if (name != NULL)
-	{
-		while (g_env != NULL)
-		{
-			i = 0;
-			while (name[i] != NULL)
-			{
-				if (ft_strequ(((t_shell_var*)(g_env->content))->name, name[i])
-						== TRUE)
-				{
-					ft_lstdelone(&g_env, &del_elem);
-					break ;
-				}
-				i++;
-			}
-			if (g_env != NULL)
-				g_env = g_env->next;
-		}
-	}
+		unset_var(name);
 	ft_free_tab(name);
-	g_env = head;
 	return (ret);
 }
