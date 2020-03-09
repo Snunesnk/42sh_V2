899fb843aedca48b0464f7c6e7b26aecaf7ee1f3
Simon Nunes <nunesimon85@gmail.com>
2020-03-09 22:48:34 +0100

    OKay now fc can open a file with hist line requested, and exec what was into this file. Need to put it at norme, and add n and r options, an test behaviour in various cases

--- src/builtins/builtin_fc.c
+++ src/builtins/builtin_fc.c
@@ -6,7 +6,7 @@
 /*   By: snunes <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/04 20:52:16 by snunes            #+#    #+#             */
-/*   Updated: 2020/03/08 22:10:37 by snunes           ###   ########.fr       */
+/*   Updated: 2020/03/09 19:46:25 by snunes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,19 +42,7 @@ static int	parse_fc_option(char ***args)
 	return (opt_list);
 }
 
-void		free_subsitute(t_sub *substitute)
-{
-	t_sub	*tmp;
 
-	while (substitute)
-	{
-		tmp = substitute->next;
-		free(substitute->pat);
-		free(substitute->rep);
-		free(substitute);
-		substitute = tmp;
-	}
-}
 
 t_sub		*init_sub(t_sub *prev_sub)
 {
@@ -84,36 +72,6 @@ t_sub		*fill_sub(t_sub *sub_list, char *pat, char *rep)
 	return (sub_list);
 }
 
-char		*ft_strreplace(char **str, char *pattern, char *replacement)
-{
-	char	*tmp;
-	int		nb_occur;
-	int		pat_len;
-	int		rep_len;
-	int		str_len;
-
-	tmp = *str;
-	nb_occur = 0;
-	pat_len = (int)ft_strlen(pattern);
-	rep_len = (int)ft_strlen(replacement);
-	str_len = (int)ft_strlen(*str);
-	while ((tmp = ft_strstr(tmp, pattern)))
-	{
-		nb_occur++;
-		tmp += pat_len;
-	}
-	if (pat_len < rep_len && !(*str = ft_memrealloc((void **)str, str_len, \
-					str_len + ((rep_len - pat_len) * nb_occur) + 1)))
-		return (NULL);
-	tmp = *str;
-	while ((tmp = ft_strstr(tmp, pattern)))
-	{
-		ft_memmove(tmp + rep_len, tmp + pat_len, ft_strlen(tmp) - pat_len + 1);
-		ft_memmove(tmp, replacement, rep_len);
-		tmp += rep_len;
-	}
-	return (*str);
-}
 
 char		*fc_do_substitute(char *str, t_sub *sub_list)
 {
@@ -134,23 +92,6 @@ char		*fc_do_substitute(char *str, t_sub *sub_list)
 		sub_list = sub_list->next;
 	}
 	return (new_cmd);
-}
-
-void		fc_replace_last_hist(char *tmp)
-{
-	unsigned int	i;
-
-	g_hist->offset = g_hist->used - 1;
-	prev_hist();
-	i = g_hist->offset;
-	while (i < g_hist->used - 1)
-	{
-		g_hist->history_content[i] = '\0';
-		i++;
-	}
-	g_hist->used = g_hist->offset + 1;
-	if (tmp)
-		add_hentry(tmp, 1);
 }
 
 int			exec_fc_s_opt(char **args)
@@ -181,143 +122,16 @@ int			exec_fc_s_opt(char **args)
 	{
 		ft_printf("./21sh: fc: no command found\n");
 		if (sub_list->pat)
-			free_subsitute(sub_list);
+			free_substitute(sub_list);
 		return (e_command_not_found);
 	}
 	if (!(tmp = fc_do_substitute(tmp, sub_list)))
 		return (e_cannot_allocate_memory);
-	free_subsitute(sub_list);
+	free_substitute(sub_list);
 	ft_dprintf(STDERR_FILENO, "%s\n", tmp);
 	fc_replace_last_hist(tmp);
 	return (exec_input(tmp));
 	return (1);
-}
-
-char		*get_editor(int opt_list, char **args)
-{
-	char	*editor;
-
-	editor = NULL;
-	if (opt_list & FC_E_OPTION)
-	{
-		editor = ft_strdup(*args);
-		(*args)++;
-	}
-	else if ((editor = getenv("FCEDIT")))
-		editor = ft_strdup(editor);
-	else if ((editor = getenv("EDITOR")))
-		editor = ft_strdup(editor);
-	else
-		editor = ft_strdup("vi");
-	if (!(editor))
-	{
-		ft_printf("./21sh: cannot allocate memory\n");
-		return (NULL);
-	}
-	return (editor);
-}
-
-void		get_hist_num(char **args, int opt_list, int *hist_end, int *hist_beg)
-{
-	char	*tmp;
-
-	tmp = prev_hist();
-	tmp = prev_hist();
-	if (!(tmp = get_beg_matching_hist(&tmp, *args)))
-		*hist_beg = -1;
-	else
-		*hist_beg = g_hist->nb_line;
-	while (g_hist->nb_line != g_hist->total_lines)
-		next_hist();
-	(*args)++;
-	if (*args)
-	{
-		if (!(tmp = get_beg_matching_hist(&tmp, *args)))
-			*hist_end = -1;
-		else
-			*hist_end = g_hist->nb_line;
-		while (g_hist->nb_line != g_hist->total_lines)
-			next_hist();
-	}
-	else
-		*hist_end = (opt_list & FC_L_OPTION) ? g_hist->total_lines - 1 : *hist_beg;
-}
-
-int			exec_fc_other_opt(int opt_list, char **args)
-{
-	char	*editor;
-	int		fd;
-	int		hist_end;
-	int		hist_beg;
-
-	if (g_hist->used == 0)
-		return (0);
-	if (!(editor = get_editor(opt_list, args)))
-		return (e_cannot_allocate_memory);
-	if (*args)
-		get_hist_num(args, opt_list, &hist_end, &hist_beg);
-	else
-	{
-		hist_end = g_hist->total_lines;
-		hist_beg = (opt_list & FC_L_OPTION) ? hist_end - 16 : hist_end;
-		if (hist_beg < 0)
-			hist_beg = 0;
-	}
-	if (!(opt_list & FC_L_OPTION))
-		fc_replace_last_hist(NULL);
-	if (hist_end < 0 || hist_beg < 0)
-	{
-		ft_printf("./21sh: fc: history specification out of range\n");
-		return (e_invalid_input);
-	}
-	if (hist_end < hist_beg)
-	{
-		int tmp;
-		tmp = hist_end;
-		hist_end = hist_beg;
-		hist_beg = tmp;
-		opt_list |= FC_R_OPTION;
-	}
-	if (opt_list & FC_L_OPTION)
-		fd = STDOUT_FILENO;
-	else
-	{
-		opt_list ^= FC_N_OPTION;
-		if (!(fd = open(".21sh_tmp_file", O_RDWR | O_CREAT | O_TRUNC, 0644)))
-		{
-			ft_printf("./21sh: cannot open temporary file\n");
-			return (1);
-		}
-	}
-	ft_printf("hist_end: %d, hist_beg: %d\n", hist_end, hist_beg);
-	while (hist_beg > g_hist->nb_line)
-		prev_hist();
-	ft_printf("hist_end: %d, hist_beg: %d\n", hist_end, hist_beg);
-	/*need to remove fc blah blah from hist*/
-	while (hist_end > hist_beg)
-	{
-		if (opt_list & FC_N_OPTION)
-			ft_dprintf(fd, "%d", g_hist->nb_line);
-		if (opt_list & FC_L_OPTION)
-			ft_dprintf(fd, "\t");
-		ft_dprintf(fd, "%s\n", g_hist->history_content + g_hist->offset + 1);
-		next_hist();
-		hist_beg++;
-	}
-	ft_printf("hist_end: %d, hist_beg: %d\n", hist_end, hist_beg);
-	if (opt_list & FC_L_OPTION)
-		return (e_success);
-	char	*command;
-	if (!(command = ft_strjoin(editor, " .21sh_tmp_file")))
-	{
-		ft_dprintf(STDERR_FILENO, "./21sh: cannot allocate memory\n");
-		return (e_cannot_allocate_memory);
-	}
-	free(editor);
-	int	status;
-	status = exec_input(command);
-	/* need to midify hist, and then exec all the commands modified */
-	return (status);
 }
 
 int			cmd_fc(int argc, char **argv)
