8f0ce3cfe955029295c94b8a81f2406624d316d9
Antoine Barthelemy <abarthel@student.42.fr>
2020-03-10 18:28:10 +0100

    fix(norm): norm on the shell on the go

--- src/init_shell.c
+++ src/init_shell.c
@@ -6,64 +6,55 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 15:32:23 by abarthel          #+#    #+#             */
-/*   Updated: 2020/03/03 15:32:27 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/03/10 18:28:02 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <sys/types.h>
-#include <signal.h>
-#include <termios.h>
-#include <unistd.h>
-#include <stdlib.h>
 
 #include "libft.h"
 #include "ft_errno.h"
 #include "shell.h"
+#include "error.h"
 
-/* Keep track of attributes of the shell. */
-
-pid_t		shell_pgid;
+pid_t			shell_pgid;
 struct termios	shell_tmodes;
-int		shell_terminal;
-int		shell_is_interactive;
-int		g_subshell = 0;
+int				shell_terminal;
+int				shell_is_interactive;
+int				g_subshell = 0;
+int				g_retval;
+char			g_pwd[] = {0};
 
-/* Return value of last program and path tracking for cd builtin */
-int             g_retval;
-char            g_pwd[] = {0};
-
-/* Make sure the shell is running interactively as the foreground job
-before proceeding. */
-
-int	init_shell(void)
+int		init_shell(void)
 {
-	/* See if we are running interactively. */
 	shell_terminal = STDIN_FILENO;
 	shell_is_interactive = isatty(shell_terminal);
-	if (shell_is_interactive) /* Loop until we are in the foreground. */
+	if (shell_is_interactive)
 	{
-		while (tcgetpgrp(shell_terminal) != (shell_pgid = getpgrp())) /* check if in foreground */
-		{
-			kill(- shell_pgid, SIGTTIN);
-		}
-		/* Ignore interactive and job-control signals. */
+		while (tcgetpgrp(shell_terminal) != (shell_pgid = getpgrp()))
+			kill(-shell_pgid, SIGTTIN);
 		init_shell_sset();
-		/* Put ourselves in our own process group. */
 		shell_pgid = getpid();
 		if (setpgid(shell_pgid, shell_pgid) < 0)
 		{
 			ft_perror("Couldn't put the shell in its own process group");
 			return (1);
 		}
-		/* Grab control of the terminal. */
 		tcsetpgrp(shell_terminal, shell_pgid);
-		/* Save default terminal attributes for shell. */
 		tcgetattr(shell_terminal, &shell_tmodes);
 	}
 	return (0);
 }
 
-int	set_minimal_env(void)
+void	set_minimal_env2(char **tmp)
+{
+	if (PATH_MAX > 0)
+	{
+		*tmp = ft_getenv("PWD");
+		if (ft_strlen(*tmp) <= PATH_MAX)
+			ft_strcpy(g_pwd, ft_getenv("PWD"));
+	}
+}
+
+int		set_minimal_env(void)
 {
 	char	*tmp;
 	int		shlvl;
@@ -85,12 +76,7 @@ int	set_minimal_env(void)
 			return (e_cannot_allocate_memory);
 		ft_memdel((void**)&tmp);
 	}
-	if (PATH_MAX > 0)
-	{
-		tmp = ft_getenv("PWD");
-		if (ft_strlen(tmp) <= PATH_MAX)
-			ft_strcpy(g_pwd, ft_getenv("PWD"));
-	}
+	set_minimal_env2(&tmp);
 	return (e_success);
 }
 
