5c175c532d784de47a493a57620836f303496d46
Simon Nunes <nunesimon85@gmail.com>
2020-03-10 14:32:27 +0100

    new file src/init_enum_tab.c + src/parser.c: norm OK

--- src/parser.c
+++ src/parser.c
@@ -6,25 +6,27 @@
 /*   By: efischer <efischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/02 16:17:27 by efischer          #+#    #+#             */
-/*   Updated: 2020/03/09 15:29:59 by efischer         ###   ########.fr       */
+/*   Updated: 2020/03/10 14:10:16 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 #include "ft_readline.h"
 
-static int	check_next_token(enum e_token type, enum e_token *token_tab)
+extern char	*g_grammar[NB_TOKEN];
+
+static int			check_next_token(enum e_token type, enum e_token *enum_tab)
 {
-	enum e_token token_index;
-	int		ret;
+	enum e_token	token_index;
+	int				ret;
 
 	ret = FAILURE;
 	token_index = 0;
-	if (token_tab != NULL)
+	if (enum_tab != NULL)
 	{
-		while (token_tab[token_index] != NONE)
+		while (enum_tab[token_index] != NONE)
 		{
-			if (type == token_tab[token_index])
+			if (type == enum_tab[token_index])
 			{
 				ret = SUCCESS;
 				break ;
@@ -35,45 +37,7 @@ static int	check_next_token(enum e_token type, enum e_token *token_tab)
 	return (ret);
 }
 
-static enum e_token **init_token_tab(void)
-{
-	static enum e_token	*token_tab[NB_TOKEN];
-
-	static enum e_token	token_pipe[] = { GREATAND, LESSAND, ANDGREAT, GREAT, LESS,
-						WORD, COMMENT, NONE };
-	static enum e_token	token_start[] = { WORD, GREATAND, LESSAND,
-						LESS, GREAT, DGREAT, DLESS, NEWLINE, IO_NB, COMMENT, END, NONE };
-	static enum e_token	token_if[] = { WORD, GREATAND, LESSAND,
-						LESS, GREAT, DGREAT, DLESS, IO_NB, COMMENT, NONE };
-	static enum e_token	token_redir[] = { WORD, NONE };
-	static enum e_token	token_word[] = { AND_IF, OR_IF, PIPE, GREATAND, LESSAND,
-						ANDGREAT, AND, SEMI, DGREAT, DLESS, GREAT, LESS, NEWLINE, WORD, IO_NB,
-						COMMENT, NONE };
-	static enum e_token	token_io_nb[] = { GREAT, LESS, DGREAT, DLESS, GREATAND, LESSAND,
-						NONE };
-
-	token_tab[SEMI] = token_start;
-	token_tab[OR_IF] = token_if;
-	token_tab[PIPE] = token_pipe;
-	token_tab[AND_IF] = token_if;
-	token_tab[GREATAND] = token_redir;
-	token_tab[LESSAND] = token_redir;
-	token_tab[ANDGREAT] = token_redir;
-	token_tab[AND] = token_start;
-	token_tab[DGREAT] = token_redir;
-	token_tab[DLESS] = token_redir;
-	token_tab[GREAT] = token_redir;
-	token_tab[LESS] = token_redir;
-	token_tab[NEWLINE] = token_start;
-	token_tab[WORD] = token_word;
-	token_tab[IO_NB] = token_io_nb;
-	token_tab[COMMENT] = token_word;
-	token_tab[START] = token_start;
-	token_tab[END] = NULL;
-	return (token_tab);
-}
-
-static void	merge_list(t_list *lst1, t_list *lst2)
+static void			merge_list(t_list *lst1, t_list *lst2)
 {
 	t_list	*tmp;
 
@@ -83,11 +47,10 @@ static void	merge_list(t_list *lst1, t_list *lst2)
 		ft_lstdelone(&lst1->next, &del);
 		ft_lstdelone(&lst2, &del);
 		lst1->next = tmp;
-		debug(lst1);
 	}
 }
 
-static int	subprompt(t_list *lst, enum e_token *token_tab)
+static int			subprompt(t_list *lst, enum e_token *enum_tab)
 {
 	t_list	*new_lst;
 	char	*tmp;
@@ -104,53 +67,53 @@ static int	subprompt(t_list *lst, enum e_token *token_tab)
 		if (ret == SUCCESS)
 		{
 			merge_list(lst, new_lst);
-			ret = check_next_token(((t_token*)(lst->next->content))->type, token_tab);
+			ret = check_next_token(((t_token*)(lst->next->content))->type,
+						enum_tab);
 		}
 		ft_strdel(&tmp);
 	}
 	return (ret);
 }
 
-
-void		parse_error(enum e_token token, char *data)
+static void			parse_error(int ret, enum e_token curr_type,
+							t_list *lst)
 {
-	char	*grammar[] = {";", "||", "|", "&&", ">&", "<&", "&>", "&", ">>",
-						"<<-", "<<", ">", "<", "newline", "IO", "Comment (#)", "Word"};
-	
+	char	*value;
 
-	ft_dprintf(2, "\n21sh: syntax error near unexpected token `%s'%s%s\n", grammar[token]
-				, token < 14 ? "" : " -> ", token < 14 ? "" : data);
+	if (ret == FAILURE)
+	{
+		value = ((t_token*)(lst->content))->value;
+		ft_dprintf(2, "\n21sh: syntax error near unexpected token `%s'%s%s\n",
+			g_grammar[curr_type], curr_type < 14 ? "" : " -> ",
+			curr_type < 14 ? "" : value);
+	}
 }
 
-int			parser(t_list *lst)
+int					parser(t_list *lst)
 {
-	static enum e_token	**token_tab = NULL;
+	static enum e_token	**enum_tab = NULL;
 	enum e_token		curr_type;
 	enum e_token		prev_type;
-	char				*value;
 	int					ret;
 
 	ret = SUCCESS;
-	if (token_tab == NULL)
-		token_tab = init_token_tab();
+	if (enum_tab == NULL)
+		enum_tab = init_enum_tab();
 	while (lst->next != NULL)
 	{
 		prev_type = ((t_token*)(lst->content))->type;
 		curr_type = ((t_token*)(lst->next->content))->type;
-		ret = check_next_token(curr_type, token_tab[prev_type]);
+		ret = check_next_token(curr_type, enum_tab[prev_type]);
 		if (ret == FAILURE && curr_type == NEWLINE)
 		{
-			ret = subprompt(lst, token_tab[prev_type]);
+			ret = subprompt(lst, enum_tab[prev_type]);
 			curr_type = ((t_token*)(lst->next->content))->type;
+			continue ;
 		}
 		if (ret == FAILURE)
 			break ;
 		lst = lst->next;
 	}
-	if (ret == FAILURE)
-	{
-		value = ((t_token*)(lst->content))->value;
-		parse_error(curr_type, value);
-	}
+	parse_error(ret, curr_type, lst);
 	return (ret);
 }
