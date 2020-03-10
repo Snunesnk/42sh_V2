17e6031deb50638c823e5744abe9df95923e7d34
Simon Nunes <nunesimon85@gmail.com>
2020-03-10 22:23:10 +0100

    fc fully functionnal and norme compliant, need to do some test files to be sure

--- src/builtins/builtin_fc.c
+++ src/builtins/builtin_fc.c
@@ -6,7 +6,7 @@
 /*   By: snunes <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/04 20:52:16 by snunes            #+#    #+#             */
-/*   Updated: 2020/03/10 20:43:38 by snunes           ###   ########.fr       */
+/*   Updated: 2020/03/10 21:41:30 by snunes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,96 +39,6 @@ static int	parse_fc_option(char ***args)
 		}
 	}
 	return (opt_list);
-}
-
-t_sub		*init_sub(t_sub *prev_sub)
-{
-	t_sub	*new_sub;
-
-	if (!(new_sub = (t_sub *)ft_memalloc(sizeof(t_sub))))
-	{
-		ft_printf("./21sh: cannot allocate memory\n");
-		return (NULL);
-	}
-	new_sub->next = NULL;
-	new_sub->prev = prev_sub;
-	new_sub->pat = NULL;
-	new_sub->rep = NULL;
-	return (new_sub);
-}
-
-t_sub		*fill_sub(t_sub *sub_list, char *pat, char *rep)
-{
-	if (!pat || !rep)
-		return (sub_list);
-	if (!(sub_list->pat = ft_strdup(pat)) || !(sub_list->rep = ft_strdup(rep)))
-	{
-		ft_printf("./21sh: cannot allocate memory\n");
-		return (NULL);
-	}
-	return (sub_list);
-}
-
-
-char		*fc_do_substitute(char *str, t_sub *sub_list)
-{
-	char	*new_cmd;
-
-	if (!(new_cmd = ft_strdup(str)))
-	{
-		ft_printf("./21sh: cannot allocate memory\n");
-		return (NULL);
-	}
-	while (sub_list && sub_list->pat && sub_list->rep)
-	{
-		if (!(new_cmd = ft_strreplace(&new_cmd, sub_list->pat, sub_list->rep)))
-		{
-			ft_printf("./21sh: cannot allocate memory\n");
-			return (NULL);
-		}
-		sub_list = sub_list->next;
-	}
-	return (new_cmd);
-}
-
-int			exec_fc_s_opt(char **args)
-{
-	char	*tmp;
-	int		status;
-	t_sub	*sub_list;
-
-	if (!(sub_list = init_sub(NULL)))
-		return (e_cannot_allocate_memory);
-	status = e_success;
-	while (*args && (tmp = ft_strchr(*args, '=')))
-	{
-		*tmp = '\0';
-		tmp += 1;
-		if (!(sub_list = fill_sub(sub_list, *args, tmp)))
-			return (e_cannot_allocate_memory);
-		if (!(sub_list->next = init_sub(sub_list)))
-			return (e_cannot_allocate_memory);
-		sub_list = sub_list->next;
-		args += 1;
-	}
-	tmp = prev_hist();
-	tmp = prev_hist();
-	while (sub_list->prev)
-		sub_list = sub_list->prev;
-	if (*args && !(get_beg_matching_hist(&tmp, *args)))
-	{
-		ft_printf("./21sh: fc: no command found\n");
-		if (sub_list->pat)
-			free_substitute(sub_list);
-		return (e_command_not_found);
-	}
-	if (!(tmp = fc_do_substitute(tmp, sub_list)))
-		return (e_cannot_allocate_memory);
-	free_substitute(sub_list);
-	ft_dprintf(STDERR_FILENO, "%s\n", tmp);
-	fc_replace_last_hist(tmp);
-	return (exec_input(tmp));
-	return (1);
 }
 
 int			cmd_fc(int argc, char **argv)
