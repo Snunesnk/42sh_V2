ebf1f2f01e31145720d40fe42dd3c0b58afc579b
Antoine Barthelemy <abarthel@student.42.fr>
2020-03-10 12:27:32 +0100

    fix(norm): readline norm on the go

--- readline/line.h
+++ readline/line.h
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 17:22:09 by abarthel          #+#    #+#             */
-/*   Updated: 2020/03/03 17:22:10 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/03/10 12:19:53 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 
 # define READLINE_DEFAULT_BUFFER_SIZE 256
 
-struct s_line
+struct	s_line
 {
 	char	*buffer;
 	size_t	len;
 };
 
-void    init_line_buffer(void);
+void	init_line_buffer(void);
 
-struct s_clipboard
+struct	s_clipboard
 {
 	char	*str;
-	int	l;
+	int		l;
 };
 
 extern struct s_clipboard	g_clip;
