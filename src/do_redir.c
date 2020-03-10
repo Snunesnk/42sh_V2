ceb9bb64d4404b0b7302f52a20855ba931ac129a
Antoine Barthelemy <abarthel@student.42.fr>
2020-03-10 16:13:12 +0100

    fix(norm): shell itself

--- src/do_redir.c
+++ src/do_redir.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 15:30:53 by abarthel          #+#    #+#             */
-/*   Updated: 2020/03/10 16:06:07 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/03/10 16:12:57 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -238,6 +238,28 @@ static int	do_iodup(t_redirection *r)
 	return (0);
 }
 
+static int	do_redir_disp(t_redirection *r)
+{
+	int	error;
+
+	error = 0;
+	if (r->instruction == IOWRITE)
+		error = do_iowrite(r);
+	else if (r->instruction == IOCAT)
+		error = do_iocat(r);
+	else if (r->instruction == IOREAD)
+		error = do_ioread(r);
+	else if (r->instruction == IOHERE)
+		error = do_iohere(r);
+	else if (r->instruction == IODUP)
+		error = do_iodup(r);
+	else if (r->instruction == (IODUP | IOREAD))
+		error = do_iodread(r);
+	else if (r->instruction == (IODUP | IOWRITE))
+		error = do_iodfile(r);
+	return (error);
+}
+
 int			do_redirection(t_redirection *r)
 {
 	int				error;
@@ -253,20 +275,7 @@ int			do_redirection(t_redirection *r)
 				undo_redirection(beg);
 			return (g_errordesc[r->error].code);
 		}
-		if (r->instruction == IOWRITE)
-			error = do_iowrite(r);
-		else if (r->instruction == IOCAT)
-			error = do_iocat(r);
-		else if (r->instruction == IOREAD)
-			error = do_ioread(r);
-		else if (r->instruction == IOHERE)
-			error = do_iohere(r);
-		else if (r->instruction == IODUP)
-			error = do_iodup(r);
-		else if (r->instruction == (IODUP | IOREAD))
-			error = do_iodread(r);
-		else if (r->instruction == (IODUP | IOWRITE))
-			error = do_iodfile(r);
+		error = do_redir_disp(r);
 		if (error)
 		{
 			if (r->flags & NOFORK)
