27792bc921b6f4fcbe2d6c7f0e0ea9f90ab1680c
Antoine Barthelemy <abarthel@student.42.fr>
2020-03-11 15:32:53 +0100

    fix(norm): shell norm on the go

--- src/signals.c
+++ src/signals.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 15:14:15 by abarthel          #+#    #+#             */
-/*   Updated: 2020/03/03 15:14:16 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/03/11 15:23:30 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 sigset_t		g_save_procmask;
 
-/* Block terminal generated signals, save old mask and set shell one */
 void	init_shell_sset(void)
 {
 	sigset_t	newmask;
 
-	/* Blocking signals terminal generated signals */
 	sigemptyset(&newmask);
 	sigaddset(&newmask, SIGINT);
 	sigaddset(&newmask, SIGQUIT);
@@ -28,10 +26,9 @@ void	init_shell_sset(void)
 	sigaddset(&newmask, SIGTTOU);
 	sigaddset(&newmask, SIGCHLD);
 	if (sigprocmask(SIG_BLOCK, &newmask, &g_save_procmask))
-		exit(1); /* Clean exit, free mem */
+		exit(1);
 }
 
-/* Restore proc mask inheritated from shell starting proc*/
 void	restore_procmask(void)
 {
 	if (sigprocmask(SIG_SETMASK, &g_save_procmask, NULL))
