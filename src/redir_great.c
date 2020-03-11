395432d0659fd271cb6a60a456be435952e146e0
Antoine Barthelemy <abarthel@student.42.fr>
2020-03-11 15:16:56 +0100

    fix(norm): shell norm on the go

--- src/redir_great.c
+++ src/redir_great.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 13:03:00 by abarthel          #+#    #+#             */
-/*   Updated: 2020/03/11 14:43:21 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/03/11 15:15:05 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,22 @@ t_redirection	*type_dgreat_redirection(t_list **lst, int io_nb)
 	return (r);
 }
 
+t_redirection	*suite_great_and(t_list **lst, t_redirection **r)
+{
+	if ((*r)->redirectee.filename[0] == '-'
+			&& (*r)->redirectee.filename[2] == '\0')
+		(*r)->flags |= FDCLOSE;
+	else if (ft_str_is_numeric((*r)->redirectee.filename))
+	{
+		(*r)->redirectee.dest = ft_atoifd((*r)->redirectee.filename);
+		(*r)->flags |= DEST;
+	}
+	else
+		(*r)->flags |= FILENAME;
+	(*lst) = (*lst)->next;
+	return (*r);
+}
+
 t_redirection	*type_greatand_redirection(t_list **lst, int io_nb)
 {
 	t_redirection	*r;
@@ -75,30 +91,5 @@ t_redirection	*type_greatand_redirection(t_list **lst, int io_nb)
 			return (r);
 		}
 	}
-	if (r->redirectee.filename[0] == '-' && r->redirectee.filename[2] == '\0')
-		r->flags |= FDCLOSE;
-	else if (ft_str_is_numeric(r->redirectee.filename))
-	{
-		r->redirectee.dest = ft_atoifd(r->redirectee.filename);
-		r->flags |= DEST;
-	}
-	else
-		r->flags |= FILENAME;
-	(*lst) = (*lst)->next;
-	return (r);
-}
-
-t_redirection	*type_andgreat_redirection(t_list **lst, int io_nb)
-{
-	t_redirection	*r;
-
-	(void)io_nb;
-	r = (t_redirection*)ft_memalloc(sizeof(t_redirection));
-	r->instruction = IODUP | IOWRITE;
-	(*lst) = (*lst)->next;
-	r->redirectee.filename = ft_strdup(get_tokvalue(*lst));
-	if (treat_single_exp(&(r->redirectee.filename), 1))
-		r->error = e_bad_substitution;
-	(*lst) = (*lst)->next;
-	return (r);
+	return (suite_great_and(lst, &r));
 }
