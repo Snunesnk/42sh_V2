2cbb5e6140543024855c1fa862ff5c6d49b63e1d
Antoine Barthelemy <abarthel@student.42.fr>
2020-03-11 15:32:53 +0100

    fix(norm): shell norm on the go

--- src/tilde_expansion.c
+++ src/tilde_expansion.c
@@ -6,11 +6,14 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/01 17:07:44 by abarthel          #+#    #+#             */
-/*   Updated: 2019/08/01 18:48:23 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/08/27 14:53:43 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "shell.h"
+#include <stdlib.h>
+
+#include "libft.h"
+#include "error.h"
 
 static int	replace_tilde(char **str, char *start, char *env)
 {
@@ -30,40 +33,45 @@ static int	replace_tilde(char **str, char *start, char *env)
 	return (e_success);
 }
 
-int	tilde_expansion(size_t *index, char **str, const char *opentag, const char *closetag)
+static void	test_syntax(size_t *index, char **str, int *ret)
 {
 	char	*env;
+
+	if (!(*str)[1] || (*str)[1] == '/')
+	{
+		env = ft_getenv("HOME");
+		*index = ft_strlen(env);
+		*ret = replace_tilde(str, &(*str)[1], env);
+	}
+	else if ((*str)[1] == '-' && ((*str)[2] == '/' || !(*str)[2]))
+	{
+		env = ft_getenv("OLDPWD");
+		*index = ft_strlen(env);
+		*ret = replace_tilde(str, &(*str)[2], env);
+	}
+	else if ((*str)[1] == '+' && ((*str)[2] == '/' || !(*str)[2]))
+	{
+		env = ft_getenv("PWD");
+		*index = ft_strlen(env);
+		*ret = replace_tilde(str, &(*str)[2], env);
+	}
+	else
+	{
+		*ret = replace_tilde(str, &(*str)[1], "~");
+		*index = 1;
+	}
+}
+
+int			tilde_expansion(size_t *index, char **str, const char *opentag,
+		const char *closetag)
+{
 	int	ret;
 
 	(void)opentag;
 	(void)closetag;
 	ret = e_success;
 	if (!*index)
-	{
-		if (!(*str)[1] || (*str)[1] == '/')
-		{
-			env = ft_getenv("HOME");
-			*index = ft_strlen(env);
-			ret = replace_tilde(str, &(*str)[1], env);
-		}
-		else if ((*str)[1] == '-' && ((*str)[2] == '/' || !(*str)[2]))
-		{
-			env = ft_getenv("OLDPWD");
-			*index = ft_strlen(env);
-			ret = replace_tilde(str, &(*str)[2], env);
-		}
-		else if ((*str)[1] == '+' && ((*str)[2] == '/' || !(*str)[2]))
-		{
-			env = ft_getenv("PWD");
-			*index = ft_strlen(env);
-			ret = replace_tilde(str, &(*str)[2], env);
-		}
-		else
-		{
-			ret = replace_tilde(str, &(*str)[1], "~");
-			*index = 1;
-		}
-	}
+		test_syntax(index, str, &ret);
 	else
 	{
 		ret = replace_tilde(str, &(*str)[1], "~");
