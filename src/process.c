72d1e9ddb103d8434683547f19b4348ea61c4d42
Antoine Barthelemy <abarthel@student.42.fr>
2020-03-11 12:06:49 +0100

    fix(norm): shell norm on the go

--- src/process.c
+++ src/process.c
@@ -6,25 +6,17 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 15:32:58 by abarthel          #+#    #+#             */
-/*   Updated: 2020/03/10 17:44:08 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/03/11 11:58:40 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stddef.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
 
 #include "libft.h"
 #include "ft_errno.h"
 #include "shell.h"
 #include "builtins.h"
 
-void	free_process(t_process *p) /* temporary for tests purposes ? */
+void	free_process(t_process *p)
 {
-//	ft_printf("\nFREE PROCESS\n"); /* Free a process */
 	free_redirections(p->redir);
 	free(p->argv);
 	free(p);
@@ -32,32 +24,23 @@ void	free_process(t_process *p) /* temporary for tests purposes ? */
 
 int	launch_process(t_process *p, pid_t pgid, int infile, int outfile, int errfile, int foreground)
 {
-//	extern char	**environ;
 	char		**envp;
 	pid_t		pid;
 	int			ret;
 
-	if (!p->argv[0]) /* Carefull and free what is needed ? */
+	if (!p->argv[0])
 		exit(1);
 	envp = get_env_tab();
 	if (shell_is_interactive)
-	{ /* Put the process into the process group and give the process group
-	  the terminal, if appropriate. This has to be done both by the shell
-	  and in the individual child processes because of potential race conditions. */
+	{
 		pid = getpid();
 		if (pgid == 0)
-		{
 			pgid = pid;
-		}
 		setpgid(pid, pgid);
 		if (!g_subshell && foreground)
 			tcsetpgrp(shell_terminal, pgid);
-		/* Set the handling for job control signals back to the default. */
 		restore_procmask();
 	}
-	/* Set the standard input/output channels of the new process. */
-
-	/* Pipeline common redirection, to minimise the dup of file descriptor */
 	if (infile != STDIN_FILENO)
 	{
 		dup2(infile, STDIN_FILENO);
@@ -73,16 +56,11 @@ int	launch_process(t_process *p, pid_t pgid, int infile, int outfile, int errfil
 		dup2(errfile, STDERR_FILENO);
 		close(errfile);
 	}
-
-	/* Sepcial redirections */
 	if ((ret = do_redirection(p->redir)))
 	{
 		free_process(p);
-		exit(g_errordesc[ret].code); /* redirection failure, error msg have to be implemented */
-	}	/* Free or not to free ? Lazy so I leave it to exit handlers */
-
-	/* Exec the new process. Make sure we exit */
-	/* Check access, etc... and send value to exit if error launching of if builtin */
+		exit(g_errordesc[ret].code);
+	}
 	ret = execute_process(p->argv, envp, NULL, NULL);
 	free_process(p);
 	exit(ret);
