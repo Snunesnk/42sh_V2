708de4f91becd08df7ca599013378a834376b2ae
Antoine Barthelemy <abarthel@student.42.fr>
2020-03-11 11:17:40 +0100

    fix(norm): shell norm on the go

--- src/job_control2.c
+++ src/job_control2.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   job_control2.c                                      :+:      :+:    :+:   */
+/*   job_control2.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 15:32:42 by abarthel          #+#    #+#             */
-/*   Updated: 2020/03/10 18:56:44 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/03/11 11:16:13 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,22 @@
 #include "shell.h"
 #include "builtins.h"
 
-int	mark_process_status(pid_t pid, int status)
+static void	mark_process_status2(pid_t pid, t_process *p, int status)
+{
+	p->status = status;
+	if (WIFSTOPPED(status))
+		p->stopped = 1;
+	else
+	{
+		p->completed = 1;
+		if (WIFSIGNALED(status))
+			ft_dprintf(STDERR_FILENO,
+			"\n%d: Terminated by signal %d.\n",
+			(int)pid, WTERMSIG(p->status));
+	}
+}
+
+int			mark_process_status(pid_t pid, int status)
 {
 	t_job		*j;
 	t_process	*p;
@@ -29,16 +44,7 @@ int	mark_process_status(pid_t pid, int status)
 			{
 				if (p->pid == pid)
 				{
-					p->status = status;
-					if (WIFSTOPPED(status))
-						p->stopped = 1;
-					else
-					{
-						p->completed = 1;
-						if (WIFSIGNALED(status))
-							ft_dprintf(STDERR_FILENO,
-			"\n%d: Terminated by signal %d.\n", (int)pid, WTERMSIG(p->status));
-					}
+					mark_process_status2(pid, p, status);
 					return (0);
 				}
 				p = p->next;
@@ -50,7 +56,7 @@ int	mark_process_status(pid_t pid, int status)
 	return (-1);
 }
 
-void	update_status(void)
+void		update_status(void)
 {
 	int		status;
 	pid_t	pid;
@@ -58,54 +64,4 @@ void	update_status(void)
 	pid = waitpid(WAIT_ANY, &status, WUNTRACED | WNOHANG);
 	while (!mark_process_status(pid, status))
 		pid = waitpid(WAIT_ANY, &status, WUNTRACED | WNOHANG);
-}
-
-void	wait_for_job(t_job *j)
-{
-	int		status;
-	pid_t	pid;
-
-	pid = waitpid(WAIT_ANY, &status, WUNTRACED);
-	while (!mark_process_status(pid, status) && !job_is_stopped(j)
-			&& !job_is_completed(j))
-		pid = waitpid(WAIT_ANY, &status, WUNTRACED);
-}
-
-void	format_job_info (t_job *j, const char *status)
-{
-	ft_dprintf(STDERR_FILENO, "%ld (%s): %s\n",
-			(long)j->pgid, status, j->command);
-}
-
-void	do_job_notification(void)
-{
-	t_job	*j;
-	t_job	*jlast;
-	t_job	*jnext;
-
-	update_status();
-	jlast = NULL;
-	j = first_job;
-	while (j)
-	{
-		jnext = j->next;
-		if (job_is_completed(j))
-		{
-			format_job_info(j, "completed");
-			if (jlast)
-				jlast->next = jnext;
-			else
-				first_job = jnext;
-			free_job(j);
-		}
-		else if (job_is_stopped(j) && !j->notified)
-		{
-			format_job_info(j, "stopped");
-			j->notified = 1;
-			jlast = j;
-		}
-		else
-			jlast = j;
-		j = jnext;
-	}
 }
