f175a3e9c59d69ad7be71d4d62a04aaff1f16724
Antoine Barthelemy <abarthel@student.42.fr>
2020-03-11 18:33:00 +0100

    fix(norm): shell norm on the go

--- src/builtins/builtins.h
+++ src/builtins/builtins.h
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 18:33:04 by abarthel          #+#    #+#             */
-/*   Updated: 2020/03/11 16:47:41 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/03/11 18:11:30 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,31 @@
 # define FNV_OFFSET 2166136261
 # define FNV_PRIME 16777619
 
+struct	s_cd
+{
+	struct stat		buf;
+	char			*path;
+	char			*oldpwd;
+	char			*tmp;
+	int				ret;
+	_Bool			p;
+};
+
+typedef struct	s_builtins
+{
+	const char *const	key;
+	int					(*const f)(int, char**);
+}				t_builtins;
+
+extern const t_builtins	g_builtins[];
+
+struct			s_keywords
+{
+	const char *const	keyword;
+};
+
 int				ft_putenv_table(char ***env, const char *name,
-				const char *value, int overwrite);
+		const char *value, int overwrite);
 int				is_a_builtin(char *cmd);
 int				cmd_unsetenv(int argc, char **argv);
 int				cmd_setenv(int argc, char **argv);
@@ -51,23 +74,21 @@ int				change_hash_entry(char *pathname, char *name);
 void			remove_hash_entry(char *name);
 int				print_error(int error_no, char *message, int ret);
 void			print_hashed_targets_util(t_hash_table *tmp, int l_option,
-				char *arg, int multiple);
+		char *arg, int multiple);
 void			print_hashed_commands_util(t_hash_table *tmp, int l_option);
 char			deal_with_p_opt(char ***args, int *x);
 char			return_next_opt(char ***args, int *x);
 int				builtins_dispatcher(char **argv);
-typedef struct	s_builtins
-{
-	const char *const	key;
-	int					(*const f)(int, char**);
-}				t_builtins;
-
-extern const t_builtins	g_builtins[];
-
-struct			s_keywords
-{
-	const char *const	keyword;
-};
+int				concatenable_operand_while(const char *str);
+int				concatenable_operand(const char *str);
+int				gfp_env(struct s_cd *cd);
+int				gfp_previous(char **argv, struct s_cd *cd);
+int				gfp_concatenable(char **argv, struct s_cd *cd);
+int				set_oldpwd(void);
+int				refresh_pwd(const char *path, _Bool p);
+int				cdpath_concat(char **path, char *env);
+int				getfullpath(char **argv, struct s_cd *cd);
+int				stat_failure(char **argv, struct s_cd *cd);
 
 extern char	*g_pathname;
 
