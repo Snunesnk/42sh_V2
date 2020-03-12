8bb6adfcdc5822ace6b9288bbd63c0cff9573d2f
Simon Nunes <nunesimon85@gmail.com>
2020-03-12 10:31:45 +0100

    new file: src/del.c; normed some functions

--- 21sh.mk
+++ 21sh.mk
@@ -6,7 +6,7 @@
 #    By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/06/25 14:45:47 by abarthel          #+#    #+#              #
-#    Updated: 2020/03/11 18:01:26 by abarthel         ###   ########.fr        #
+#    Updated: 2020/03/12 09:53:03 by efischer         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,6 +20,7 @@ SOURCES :=	main.c \
 		build_redir.c \
 		debug.c \
 		debug_ast.c \
+		del.c \
 		do_redir.c \
 		do_redir2.c \
 		do_redir3.c \
