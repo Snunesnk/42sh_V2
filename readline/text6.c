c9ee1e3acee8c7d4eee30ff08676cba799d1f67f
Antoine Barthelemy <abarthel@student.42.fr>
2020-03-15 22:02:41 +0100

    feat(ctrl): add ctrl + x - x

--- readline/text6.c
+++ readline/text6.c
@@ -4,8 +4,12 @@ _Bool	g_ctrl_mode = 0;
 
 static void	switch_cbpos(void)
 {
-	insert_text("Hello", 5);
-	return ;
+	int		tmp;
+
+	tmp = g_dis.cbpos;
+	g_dis.cbpos = g_dis.cbpos_prev;
+	g_dis.cbpos_prev = tmp;
+	update_line();
 }
 
 void	rl_ctrl_mode(union u_buffer u)
