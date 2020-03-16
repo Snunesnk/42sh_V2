68a8b84dc404f20e3d90d2c1229baeb54f593abe
Simon Nunes <nunesimon85@gmail.com>
2020-03-16 17:54:37 +0100

    add avl tree function to store future completions

---
+++ readline/avl_utils.c
@@ -0,0 +1,20 @@
+#include "avl.h"
+
+void	free_node(t_node *node)
+{
+	if (!node)
+		return ;
+	if (node->left)
+		free_node(node->left);
+	free(node->name);
+	if (node->right)
+		free_node(node->right);
+	free(node);
+}
+
+int		max(int a, int b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
