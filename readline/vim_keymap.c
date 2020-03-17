7d63f16a604463aa50cf4a6dcc45a12220972999
Antoine Barthelemy <abarthel@student.42.fr>
2020-03-17 21:52:19 +0100

    feat(binding): adding vim binding

--- readline/vim_keymap.c
+++ readline/vim_keymap.c
@@ -3,16 +3,34 @@
 /* Insert mode */
 struct s_keymap_entry g_vim_standard_insert_keymap[] =
 {
-	[ 0 ... 26 ] = { ISFUNC, rl_void },
+	[ 0 ... 22 ] = { ISFUNC, rl_void },
+	{ ISFUNC, cut_prev_wd },
+	[ 24 ... 26 ] = { ISFUNC, rl_void },
 	{ ISKMAP, vim_escape }, /* Escape */
 	[ 28 ... 31 ] = { ISFUNC, rl_void },
 	[ ' ' ... '~' ] = { ISFUNC, rl_insert },
-	{ ISFUNC, rl_void }
+	{ ISFUNC, rl_backspace }	
 };
 
 struct s_keymap_entry g_vim_ctlx_insert_keymap[] =
 {
-	[ 0 ... 127 ] = { ISFUNC, rl_void }
+	[ 0 ... 50 ] = { ISFUNC, rl_void },
+	{ ISFUNC, rl_delete },
+	[ 52 ... 64 ] = { ISFUNC, rl_void },
+	{ ISFUNC, history_up },
+	{ ISFUNC, history_down },
+	{ ISFUNC, cursor_r },
+	{ ISFUNC, cursor_l },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_end },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_home },
+	[ 73 ... 84 ] = { ISFUNC, rl_void },
+	{ ISFUNC, cursor_u },
+	{ ISFUNC, cursor_d },
+	{ ISFUNC, wd_right },
+	{ ISFUNC, wd_left },
+	[ 89 ... 127 ] = { ISFUNC, rl_void }
 };
 
 struct s_keymap_entry g_vim_meta_insert_keymap[] =
