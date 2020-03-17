fe61985d061e132ecca5814011d8e00e65d52ae8
Antoine Barthelemy <abarthel@student.42.fr>
2020-03-17 21:22:19 +0100

    feat(bind): adding vim binding

--- readline/vim_keymap.c
+++ readline/vim_keymap.c
@@ -1,5 +1,6 @@
 #include "ft_readline.h"
 
+/* Insert mode */
 struct s_keymap_entry g_vim_standard_insert_keymap[] =
 {
 	[ 0 ... 26 ] = { ISFUNC, rl_void },
@@ -11,33 +12,28 @@ struct s_keymap_entry g_vim_standard_insert_keymap[] =
 
 struct s_keymap_entry g_vim_ctlx_insert_keymap[] =
 {
-	[ 0 ... 104 ] = { ISFUNC, rl_void },
-	{ ISFUNC, vim_insert },
-	[ 106 ... 127 ] = { ISFUNC, rl_void },
+	[ 0 ... 127 ] = { ISFUNC, rl_void }
 };
 
 struct s_keymap_entry g_vim_meta_insert_keymap[] =
 {
-	[ 0 ... 127 ] = { ISFUNC, rl_void },
+	[ 0 ... 127 ] = { ISFUNC, rl_void }
 };
 
+/* Command mode */
 struct s_keymap_entry g_vim_standard_cmd_keymap[] =
 {
-	[ 0 ... 26 ] = { ISFUNC, rl_void },
-	{ ISKMAP, vim_escape }, /* Escape */
-	[ 28 ... 31 ] = { ISFUNC, rl_void },
-	[ ' ' ... '~' ] = { ISFUNC, rl_insert },
-	{ ISFUNC, rl_void }
+	[ 0 ... 104 ] = { ISFUNC, rl_void },
+	{ ISFUNC, vim_insert },
+	[ 106 ... 127 ] = { ISFUNC, rl_void }
 };
 
 struct s_keymap_entry g_vim_ctlx_cmd_keymap[] =
 {
-	[ 0 ... 104 ] = { ISFUNC, rl_void },
-	{ ISFUNC, vim_insert },
-	[ 106 ... 127 ] = { ISFUNC, rl_void },
+	[ 0 ... 127 ] = { ISFUNC, rl_void }
 };
 
 struct s_keymap_entry g_vim_meta_cmd_keymap[] =
 {
-	[ 0 ... 127 ] = { ISFUNC, rl_void },
+	[ 0 ... 127 ] = { ISFUNC, rl_void }
 };
