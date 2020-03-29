6d23c6790b8c0ce48aaab320962307123156a416
Antoine Barthelemy <abarthel@student.42.fr>
2020-03-29 13:44:25 +0200

    fix(norm): fixing norm for finals

--- libft/include/ft_stack.h
+++ libft/include/ft_stack.h
@@ -6,24 +6,24 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 14:09:58 by abarthel          #+#    #+#             */
-/*   Updated: 2019/10/22 14:15:20 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/03/29 13:43:40 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_STACK_H
 # define FT_STACK_H
 
-struct	s_stack
+struct			s_stack
 {
 	struct s_stack	*previous;
-	void		*data;	
+	void			*data;
 };
 
 struct s_stack	*stack_create_element(void *data);
 struct s_stack	*stack_initialize(void *data);
 void			stack_push(struct s_stack **top, void *data);
 void			stack_pop(struct s_stack **top, void (*del_data)(void *));
-void 			*stack_pop_get_data(struct s_stack **top);
+void			*stack_pop_get_data(struct s_stack **top);
 void			*stack_peek(struct s_stack **top);
 void			stack_swap(struct s_stack **top);
 void			stack_delete(struct s_stack **top, void (*del_data)(void *));
