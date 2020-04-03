144d3f3f29dab5d669d1be03df83edd1356239cc
Simon Nunes <nunesimon85@gmail.com>
2020-04-03 23:05:48 +0200

    Displaying of result is ok. Need to add highlight, navigation and insert

--- readline/fill_data.c
+++ readline/fill_data.c
@@ -8,11 +8,10 @@ t_data	*init_data(void)
 		return (NULL);
 	new_data->name_l = 0;
 	new_data->column = 0;
+	new_data->chosen_exec = 0;
+	new_data->first_print = 0;
+	new_data->last_print = 0;
 	new_data->nb_exec = 0;
-	new_data->exec_lim = 0;
-	new_data->start = 0;
-	new_data->end = 0;
-	new_data->nb_exec_sav = 0;
 	new_data->type = 0;
 	return (new_data);
 }
@@ -41,21 +40,33 @@ void	get_cursor_position(int *column, int *row)
 	}
 }
 
-int		get_exec_lim(t_data *data)
+void		get_exec_lim(t_data *data)
 {
-	int	column;
-	int	row;
-	int	area;
-	int	exec_lim;
+	int				chosen_exec_line;
+	int				middle_line;
 
-	area = data->nb_line * data->name_p_line;
-	if (area != data->exec_lim)
-		exec_lim = data->nb_line * (data->name_p_line - 1);
+	chosen_exec_line = data->chosen_exec % data->nb_line;
+	if (data->nb_line < data->row -1)
+	{
+		data->first_print = 1;
+		data->last_print = data->nb_line;
+	}
 	else
-		exec_lim = area;
-	get_cursor_position(&column, &row);
-	ft_printf("pos du cursor: [%d, %d]\n", row, column);
-	return (exec_lim);
+	{
+		middle_line = (data->row - 1) / 2;
+		data->first_print = chosen_exec_line - middle_line;
+		data->last_print = chosen_exec_line + middle_line;
+		if (data->first_print < 1)
+		{
+			data->last_print -= (data->first_print - 1);
+			data->first_print -= (data->first_print - 1);
+		}
+		if (data->last_print > data->nb_line)
+		{
+			data->first_print -= data->last_print - data->nb_line;
+			data->last_print -= data->last_print - data->nb_line;
+		}
+	}
 }
 
 int		get_nb_exec(t_node *compl_tree, int nb_exec)
@@ -73,24 +84,22 @@ int		get_nb_exec(t_node *compl_tree, int nb_exec)
 
 t_data	*fill_data(t_data *data, t_node *compl_tree)
 {
-	data->curs_col = 0;
-	data->curs_row = 0;
 	data->nb_line = 1;
 	data->name_p_line = 0;
-	data->nb_exec_sav = get_nb_exec(compl_tree, 0);
+	data->nb_exec = get_nb_exec(compl_tree, 0);
 	if (data->name_l)
 		data->name_p_line = data->column / data->name_l;
 	if (data->name_p_line)
-		data->nb_line = data->nb_exec_sav / data->name_p_line + 1;
-	if (data->name_p_line > data->nb_exec_sav / data->nb_line)
+		data->nb_line = data->nb_exec / data->name_p_line + 1;
+	if (data->name_p_line > data->nb_exec / data->nb_line)
 	{
-		if ((data->nb_exec_sav / data->nb_line) * data->nb_line \
-				!= data->nb_exec_sav)
-			data->name_p_line = data->nb_exec_sav / data->nb_line + 1;
+		if ((data->nb_exec / data->nb_line) * data->nb_line \
+				!= data->nb_exec)
+			data->name_p_line = data->nb_exec / data->nb_line + 1;
 		else
-			data->name_p_line = data->nb_exec_sav / data->nb_line;
+			data->name_p_line = data->nb_exec / data->nb_line;
 	}
-	data->exec_lim = get_exec_lim(data);
-	ft_printf("nb_exec: %d\n", data->nb_exec_sav);
+	get_exec_lim(data);
+	ft_printf("nb_exec: %d\n", data->nb_exec);
 	return (data);
 }
--- readline/print_cmpl_tree.c
+++ readline/print_cmpl_tree.c
@@ -3,24 +3,31 @@
 void	print_compl(t_node *compl_tree, t_data *data)
 {
 	int	to_print;
-//	int	area;
+	int	line;
 
-//	area = data->nb_line * data->name_p_line;
-	to_print = 1;
-	ft_printf("name_l: %d, nb_exec: %d\n", data->name_l, data->nb_exec);
-	ft_printf("nb_line: %d, name_p_line: %d\n", data->nb_line, data->name_p_line);
-	while (to_print != data->exec_lim)
+	ft_putstr(g_termcaps.cd);
+	line = 0;
+	to_print = data->first_print;
+//	ft_printf("name_l: %d, chosen_exec: %d\n", data->name_l, data->chosen_exec);
+//	ft_printf("nb_line: %d, name_p_line: %d\n", data->nb_line, data->name_p_line);
+//	ft_printf("first print: %d, last_print: %d\n", data->first_print, data->last_print);
+	while (line < data->last_print)
 	{
+//		ft_printf("to_print: %d\n", to_print);
 		if (to_print)
 			print_tree(compl_tree, data, to_print);
 		to_print = to_print + data->nb_line;
-		if (to_print > data->nb_exec_sav)
-			ft_putstr("\n");
-		to_print = to_print % (data->nb_exec_sav + 1);
+//		ft_printf("to_print: %d\n", to_print);
+		if (to_print > data->nb_exec)
+		{
+			line++;
+			to_print = data->first_print + line;
+			if (line < data->last_print)
+				ft_putstr("\n");
+		}
 	}
-	if (to_print)
-		print_tree(compl_tree, data, to_print);
-	ft_putstr("\n");
+	ft_putstr(tgoto(g_termcaps.gup, 0, line));
+	update_line();
 }
 
 void	print_tree(t_node *compl_tree, t_data *data, int to_print)
