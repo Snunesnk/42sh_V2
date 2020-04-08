b6762560d43b695451768fb66f835eb3f3835691
Simon Nunes <nunesimon85@gmail.com>
2020-04-08 23:08:44 +0200

    file and path completion done, still need to fix slash in g_line

--- readline/autocomplete.h
+++ readline/autocomplete.h
@@ -8,12 +8,15 @@
 # define ARROW_LEFT		4479771
 # define ARROW_RIGHT	4414235
 
+# define SPACES "                                                           "
+
 int				g_autocompl_on;
 int				g_autocompl_query;
 union u_buffer	g_autocompl_bad_seq;
 
 void	autocomplete(void);
 t_node	*get_cmd_compl(char *to_complete, char *path, t_data *data);
+t_node	*get_file_compl(char *to_complete, t_data *data);
 t_data	*init_data(void);;
 t_data	*fill_data(t_data *data, t_node *compl_tree);
 void	print_tree(t_node *compl_tree, t_data *data, int to_print);
@@ -21,5 +24,8 @@ void	print_compl(t_node *compl_tree, t_data *data);
 void	update_exec(union u_buffer c, t_data *data);
 void	display_compl(t_node *compl_tree, t_data *data);
 int		calc_left_arrow(t_data * data);
+int		ft_str_isspace(char *str);
+void	insert_compl(t_node *compl_tree, t_data *data);
+size_t	ft_str_wchar_len(char *str);
 
 #endif
