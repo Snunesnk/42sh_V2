4271809743eec0bd31ee4e837b4b96022d6f8b1a
Simon Nunes <nunesimon85@gmail.com>
2020-04-11 22:05:26 +0200

    fix bug when last completion wasnt reacheable when using left arrow

--- readline/fill_data_util.c
+++ readline/fill_data_util.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/09 13:38:15 by abarthel          #+#    #+#             */
-/*   Updated: 2020/04/09 13:38:16 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/04/11 22:03:29 by snunes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	calc_left_arrow(t_data *data)
 	if (line == 1)
 	{
 		data->chosen_exec = data->nb_line;
-		while (data->chosen_exec < data->nb_exec)
+		while (data->chosen_exec <= data->nb_exec)
 			data->chosen_exec += data->nb_line;
 		return (data->chosen_exec - data->nb_line);
 	}
