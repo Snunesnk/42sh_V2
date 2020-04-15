2fea9b9561390e6990a2b710a05a8a0a6d95626c
Simon Nunes <nunesimon85@gmail.com>
2020-04-15 12:55:42 +0200

    add a more user-friendly prompt

--- readline/get_prompt.c
+++ readline/get_prompt.c
@@ -6,14 +6,14 @@
 /*   By: snunes <snunes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/14 21:10:46 by snunes            #+#    #+#             */
-/*   Updated: 2020/04/14 22:58:39 by snunes           ###   ########.fr       */
+/*   Updated: 2020/04/15 12:53:53 by snunes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_readline.h"
 #include "shell.h"
 
-size_t	get_vis_prompt_len(char *prompt)
+size_t		get_vis_prompt_len(char *prompt)
 {
 	size_t	len;
 	int		i;
@@ -40,18 +40,57 @@ size_t	get_vis_prompt_len(char *prompt)
 	return (len);
 }
 
-char	*get_prompt(void)
+static int	remove_home(char **pwd)
+{
+	char	*home;
+	char	*tmp;
+
+	if (!(home = ft_getenv("HOME")))
+		return (e_success);
+	if (!(tmp = ft_strstr(*pwd, home)))
+		return (e_success);
+	if (!(*pwd = ft_strdup(*pwd)))
+	{
+		ft_dprintf(STDERR_FILENO, "./21sh: cannot allocate memory\n");
+		return (e_cannot_allocate_memory);
+	}
+	tmp = ft_strstr(*pwd, home);
+	tmp[0] = '\0';
+	tmp = ft_strcat(tmp, tmp + ft_strlen(home) - 1);
+	tmp[0] = '~';
+	if (!tmp[1])
+	{
+		tmp[1] = 0;
+		tmp[2] = 0;
+	}
+	return (e_success);
+}
+
+char		*get_prompt(void)
 {
 	char	*prompt;
+	char	*pwd;
+	int		len;
 
-	prompt = ft_strdup("\033[38;5;196m\u279c \033[0m");
+	if (!(pwd = ft_getenv("PWD")))
+		return (ft_strdup("21sh$ "));
+	len = ft_strlen(pwd) + ft_strlen(GREEN_ARROW) + ft_strlen(PROMPT_COLOR) \
+		  + ft_strlen(END_SIGN) + 1;
+	if (!(prompt = (char *)ft_memalloc(sizeof(char) * (len + 1))))
+	{
+		ft_dprintf(STDERR_FILENO, "./21sh: cannot allocate memory\n");
+		return (NULL);
+	}
 	if (g_retval)
-		write(1, "\033[38;5;196m\u279c \n", sizeof("\033[38;5;196m\u279c" + 1));
+		ft_strcat(prompt, RED_ARROW);
 	else
-		write(1, "\033[38;5;196m\u279c", sizeof("\033[38;5;196m\u279c"));
-	//	add_green_arrow
-//	add_pwd
-//	replace_home
-//	and why not short it
+		ft_strcat(prompt, GREEN_ARROW);
+	ft_strcat(prompt, PROMPT_COLOR);
+	if (remove_home(&pwd))
+		return (NULL);
+	ft_strcat(prompt, pwd);
+	if (!ft_strequ(pwd, ft_getenv("PWD")))
+		free(pwd);
+	ft_strcat(prompt, END_SIGN);
 	return (prompt);
 }
