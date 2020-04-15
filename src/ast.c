a582577e7d22abddb3e4fa4640796c5fa2b663c7
Antoine Barthelemy <abarthel@student.42.fr>
2020-04-15 17:18:28 +0200

    fix(ast): building ast using the 2 stack method

--- src/ast.c
+++ src/ast.c
@@ -3,66 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ast.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: efischer <efischer@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/12 10:39:25 by efischer          #+#    #+#             */
-/*   Updated: 2020/04/14 18:19:57 by abarthel         ###   ########.fr       */
+/*   Created: 2020/04/15 17:04:31 by abarthel          #+#    #+#             */
+/*   Updated: 2020/04/15 17:15:59 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "shell.h"
 
-static t_list	*remove_border(t_list **lst)
+t_ast			*build_ast(t_list *lst)
 {
-	t_list	*next;
-	t_list	*tmp;
+	t_ast	*ast;
 
-	next = (*lst)->next;
-	ft_lstdelone(lst, &del);
-	*lst = next;
-	tmp = next;
-	while (tmp != NULL && tmp->next != NULL && tmp->next->next != NULL
-		&& tmp->next->next->next != NULL)
-	{
-		tmp = tmp->next;
-	}
-	ft_lstdelone(&(tmp->next->next), &del);
-	ft_lstdelone(&(tmp->next), &del);
-	tmp->next = NULL;
-	return (next);
-}
-
-int				build_ast(t_ast **ast, t_list *lst)
-{
-	t_list	*head;
-	t_list	*tmp;
-	int		type;
-
-	head = remove_border(&lst);
-	tmp = lst;
-	while (head)
-	{
-		type = lst ? ((t_token*)(lst->content))->type : 0;
-		if (type && (type == AND || type == AND_IF
-			|| type == OR_IF || type == SEMI))
-		{
-			if (!lst)
-				tmp = NULL;
-			if (new_node_ast(ast, head, &tmp, type) == FAILURE)
-				return (FAILURE);
-			head = tmp;
-			lst = head;
-			tmp = lst;
-			continue ;
-		}
-		lst = lst->next;
-	}
-	ft_printf("\n---------BEFORE-------\n");
-	debug_ast(*ast);
-	ft_printf("\n---------AFTER-------\n");
-	ast_order(ast);
-	debug_ast(*ast);
-	ft_printf("\n---NEXT---\n");
-	return (SUCCESS);
+//	debug_ast(*ast);
+	return (ast);
 }
