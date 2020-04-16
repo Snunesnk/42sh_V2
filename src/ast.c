950d4754d34c8f237da116308f50956d7e4dd6a4
Antoine Barthelemy <abarthel@student.42.fr>
2020-04-16 13:17:33 +0200

    fix(ast): adding new lexer, new parser, new ast

--- src/ast.c
+++ src/ast.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/15 17:04:31 by abarthel          #+#    #+#             */
-/*   Updated: 2020/04/16 13:07:04 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/04/16 13:17:20 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,10 @@ static t_ast		*build_node(t_list **lst)
 	int		type;
 
 	pipeline = build_leaf(lst);
-//	debug(*lst);
 	type = ((t_token*)((*lst)->content))->type;
 	if (type  == AND_IF || type  == OR_IF)
 	{
 		(*lst) = (*lst)->next;
-//		ft_printf("\n|%s|\n", g_tokval[type]);
 		return (alloc_node(type, NULL,
 			alloc_node(WORD, pipeline, NULL, NULL),
 			build_node(lst)));
@@ -60,11 +58,7 @@ t_ast			*build_ast(t_list **lst)
 	if (((t_token*)((*lst)->content))->type == NEWLINE)
 		return (NULL);
 	ast = build_node(lst);
-//	ft_printf("\n--START---\n");
-//	debug_ast(ast);
-//	ft_printf("\n--END---\n");
 	type = ((t_token*)((*lst)->content))->type;
-//	ft_printf("\n|%s|\n", g_tokval[type]);
 	if (type == SEMI || type == AND)
 	{
 		(*lst) = (*lst)->next;
