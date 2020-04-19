a3523756eafe6cd9227064004834bd70374284cd
Antoine Barthelemy <abarthel@student.42.fr>
2020-04-19 15:20:42 +0200

    fix(leaks): without script, there is no need to dup value of token in redir etc

--- src/lexer_utils.c
+++ src/lexer_utils.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/16 13:54:57 by abarthel          #+#    #+#             */
-/*   Updated: 2020/04/16 13:54:58 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/04/19 15:18:46 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ char	*ft_join_free(char *s1, char *s2, int op)
 
 	str = ft_strjoin(s1, s2);
 	if (op == 1)
-		ft_strdel(&s1);
+		free(s1);
 	else if (op == 2)
-		ft_strdel(&s2);
+		free(s2);
 	else
 	{
-		ft_strdel(&s1);
-		ft_strdel(&s2);
+		free(s1);
+		free(s2);
 	}
 	return (str);
 }
