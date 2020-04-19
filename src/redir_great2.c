fb951cf76ae523e22311dc9bc379d78550fda955
Antoine Barthelemy <abarthel@student.42.fr>
2020-04-19 14:50:05 +0200

    fix(leaks): without script, there is no need to dup value of token in redir etc

--- src/redir_great2.c
+++ src/redir_great2.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 13:03:00 by abarthel          #+#    #+#             */
-/*   Updated: 2020/03/11 15:14:42 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/04/19 14:46:44 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ t_redirection	*type_andgreat_redirection(t_list **lst, int io_nb)
 	r = (t_redirection*)ft_memalloc(sizeof(t_redirection));
 	r->instruction = IODUP | IOWRITE;
 	(*lst) = (*lst)->next;
-	r->redirectee.filename = ft_strdup(get_tokvalue(*lst));
+	r->redirectee.filename = get_tokvalue(*lst);
 	if (treat_single_exp(&(r->redirectee.filename), 1))
 		r->error = e_bad_substitution;
 	(*lst) = (*lst)->next;
