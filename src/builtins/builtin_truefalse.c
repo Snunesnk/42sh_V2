127a939d559ad8697c94d3d5df33b7f10182abfb
Yoann Foreau <yoyo.fofo47@gmail.com>
2020-04-21 08:45:01 +0200

    Fix builtin_truefalse (reverse them)
    This does not fix the command 'true || echo foo && echo bar', which
    doesn't print anything now.

--- src/builtins/builtin_truefalse.c
+++ src/builtins/builtin_truefalse.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 20:52:32 by abarthel          #+#    #+#             */
-/*   Updated: 2019/07/21 19:13:09 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/04/21 08:42:27 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ int	cmd_true(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
-	return (1);
+	return (0);
 }
 
 int	cmd_false(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
-	return (0);
+	return (1);
 }
