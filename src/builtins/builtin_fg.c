f37d8181efe57f492da50d0c4885cf3fdb3b6c98
Antoine Barthelemy <abarthel@student.42.fr>
2020-04-27 11:59:37 +0200

    feat(debug job_list): adding algo printing of job list remaining

--- src/builtins/builtin_fg.c
+++ src/builtins/builtin_fg.c
@@ -6,15 +6,12 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 20:52:32 by abarthel          #+#    #+#             */
-/*   Updated: 2020/04/27 10:49:55 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/04/27 11:59:05 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 #include "builtins.h"
-
-void            print_jobs(void);
-
 
 t_job	*find_lastbackgrounded(void)
 {
@@ -50,7 +47,7 @@ int		cmd_fg(int argc, char **argv)
 			return (1);
 		}
 	}
-	if (argc == 2)
+	else if (argc == 2)
 	{
 		j = find_job(ft_atoi(argv[1]));
 		if (j == NULL)
@@ -61,9 +58,7 @@ int		cmd_fg(int argc, char **argv)
 		put_job_in_foreground(j, 1);
 	}
 	else
-	{
 		return (pbierror("usage: %s [job_spec]",
 					g_builtin_name));
-	}
 	return (0);
 }
