901413352aace289f961b770dd62ba0cae991241
Antoine Barthelemy <abarthel@student.42.fr>
2020-04-28 15:16:20 +0200

    fix(norm): removed ranges in emacs binding

--- readline/vim_keymap.c
+++ readline/vim_keymap.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/09 13:35:56 by abarthel          #+#    #+#             */
-/*   Updated: 2020/04/28 13:57:34 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/04/28 15:14:01 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,37 @@
 
 struct s_keymap_entry g_vim_standard_insert_keymap[] =
 {
-	[ 0 ... 2 ] = { ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
 	{ ISFUNC, kill_line },
 	{ ISFUNC, rl_eot },
-	[ 5 ... 8 ] = { ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
 	{ ISFUNC, autocomplete },
-	[ 10 ... 22 ] = { ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
 	{ ISFUNC, cut_prev_wd },
-	[ 24 ... 26 ] = { ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
 	{ ISKMAP, vim_escape },
-	[ 28 ... 31 ] = { ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
+	{ ISFUNC, rl_void },
 	[ ' ' ... '~' ] = { ISFUNC, rl_insert },
 	{ ISFUNC, rl_backspace }
 };
