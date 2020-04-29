f14a14d606dcf2d9fadd645e84611808f6e925af
Simon Nunes <nunesimon85@gmail.com>
2020-04-29 20:22:17 +0200

    start fixing hist newline

--- readline/display_utils.c
+++ readline/display_utils.c
@@ -6,7 +6,7 @@
 /*   By: snunes <snunes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/24 13:21:12 by snunes            #+#    #+#             */
-/*   Updated: 2020/04/27 12:36:33 by snunes           ###   ########.fr       */
+/*   Updated: 2020/04/29 20:19:50 by snunes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,13 @@ void	get_start_offset(void)
 		i++;
 	}
 	g_dis.start_offset -= (g_dis.start_offset) ? 1 : 0;
+}
+
+void	count_newline(void)
+{
+	return ;
+	//int	i;
+
+	//i = 0;
+	//g_cursor.v_pos = 0;
 }
