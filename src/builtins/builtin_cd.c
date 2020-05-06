696b9b6a495afca2e23b0b970c6838ca949df048
Antoine Barthelemy <abarthel@student.42.fr>
2020-05-06 15:33:55 +0200

    fix(cd): fixing cdpath concat leaks before continuing algo

--- src/builtins/builtin_cd.c
+++ src/builtins/builtin_cd.c
@@ -26,7 +26,7 @@ static int	cd_parse_opt(int argc, char **argv, _Bool *p)
 	return (e_success);
 }
 /*
-int	concatenable_operand_while(const char *str)
+static int	concatenable_operand_while(const char *str)
 {
 	while (*str)
 	{
@@ -37,7 +37,7 @@ int	concatenable_operand_while(const char *str)
 	return (0);
 }
 
-int	concatenable_operand(const char *str)
+static int	concatenable_operand(const char *str)
 {
 	if (*str == '.')
 	{
@@ -60,6 +60,7 @@ static int	go_home(_Bool p_option)
 	char	*home;
 
 	home = get_shell_var("HOME", g_env);
+//	1. If no directory operand is given and the HOME environment variable is empty or undefined, the default behavior is implementation-defined and no further steps shall be taken.
 	if (!home)
 	{
 		pbierror("HOME not set");
@@ -68,8 +69,11 @@ static int	go_home(_Bool p_option)
 	else if (!home[0])
 		return (0);
 	else
+//	2. If no directory operand is given and the HOME environment variable is set to a non-empty value, the cd utility shall behave as if the directory named in the HOME environment variable was specified as the directory operand.
 		return (cd_internal(home, p_option));
 }
+
+
 /*
 int	gfp_previous(struct s_cd *cd)
 { // Case it goes previous: cd -
@@ -95,28 +99,40 @@ int	gfp_previous(struct s_cd *cd)
 	ft_printf("%s\n", cd->path);
 	return (0);
 }
-
-int	gfp_concatenable(char **argv, struct s_cd *cd)
+*/
+static char	*concatenate_cdpath(char *directory)
 {
-	cd->path = ft_strdup(argv[g_optind]);
-	if ((cd->ret = cdpath_concat(&(cd->path),
-					NULL)) == e_cannot_allocate_memory)
-		return (g_errordesc[e_cannot_allocate_memory].code);
-	else if (cd->ret == 3)
-	{
-		cd->path = ft_resolvepath(cd->path);
-		ft_printf("%s\n", cd->path);
-	}
-	else
-	{
-		cd->tmp = cd->path;
-		cd->path = ft_strnjoin(3, g_pwd, "/", cd->tmp);
-		ft_memdel((void**)&(cd->tmp));
-	}
-	cd->ret = e_success;
-	return (0);
-}
+	char	*cdpath;
+	char	*cdpath_origin;
+	char	*dir;
+	char	*pathname;
 
+//	5. Starting with the first pathname in the <colon>-separated pathnames of CDPATH
+//	   (see the ENVIRONMENT VARIABLES section) if the pathname is non-null, test if the concatenation of that pathname,
+//	   a <slash> character if that pathname did not end with a <slash> character, and the directory operand names
+//	   a directory. If the pathname is null, test if the concatenation of dot, a <slash> character, and the operand
+//	   names a directory. In either case, if the resulting string names an existing directory, set curpath to that
+//	   string and proceed to step 7. Otherwise, repeat this step with the next pathname in CDPATH until all
+//	   pathnames have been tested.
+	cdpath = ft_strdup(get_shell_var("CDPATH", g_env));
+	cdpath_origin = cdpath;
+	if (!cdpath || !cdpath[0])
+		return (NULL);
+	while ((dir = ft_strsep(&cdpath, ":")))
+	{
+		pathname = ft_strnjoin(3, dir, "/", directory);
+		pathname = ft_resolvepath(pathname);
+		ft_printf("test: %s\n", pathname); // DEBUGG
+		if (!access(pathname, X_OK))
+			break ;
+	}
+	ft_printf("returned cdpath: %s\n", pathname); // DEBUGG
+	if (pathname)
+		ft_printf("%s\n", pathname);
+	ft_memdel((void**)&cdpath_origin);
+	return (pathname);
+}
+/*
 int	set_oldpwd(void)
 {
 	char	*cwd;
@@ -165,36 +181,8 @@ int	refresh_pwd(const char *path, _Bool p)
 	}
 	return (0);
 }
-
-int	cdpath_concat(char **path, char *env)
-{
-	char	*beg;
-	char	*dir;
-	char	*pathname;
-
-	if (!(beg = get_shell_var("CDPATH", g_env)) || !beg[0])
-		return (e_success);
-	if (!(env = ft_strdup(beg)))
-		return (e_cannot_allocate_memory);
-	beg = env;
-	while ((dir = ft_strsep(&env, ":")))
-	{
-		if (!(pathname = ft_strnjoin(3, dir, "/", *path)))
-			return (e_cannot_allocate_memory);
-		if (!access(pathname, X_OK))
-			break ;
-		ft_memdel((void**)&pathname);
-	}
-	ft_memdel((void**)&beg);
-	if (dir)
-	{
-		ft_memdel((void**)path);
-		*path = pathname;
-		return (3);
-	}
-	return (e_success);
-}
-
+*/
+/*
 int	stat_failure(char **argv, struct s_cd *cd)
 {
 	if (!argv[g_optind] || !*argv[g_optind])
@@ -247,8 +235,60 @@ static int	changedir_failure(struct s_cd *cd)
 
 int	cd_internal(char *directory, _Bool p_option)
 {
+	char	*curpath;
+	char	*cdpath;
+
+	curpath = NULL;
+	cdpath = NULL;
+//	1. & 2. if no dir, go_home (step 1. and 2.)
 	if (!directory) // Case: cd | cd -L | cd -P
 		return (go_home(p_option));
+//	3. if the directory operand begins with a <slash> character, set curpath to the operand and proceed to step 7.
+	else if (directory[0] == '/')
+		curpath = directory;
+//	4. If the first component of the directory operand is dot or dot-dot, proceed to step 6.
+	else if (!ft_strcmp(directory, ".") || !ft_strcmp(directory, ".."))
+		(void)directory;
+//	5. Starting with the first pathname in the <colon>-separated pathnames of CDPATH
+//	   (see the ENVIRONMENT VARIABLES section) if the pathname is non-null, test if the concatenation of that pathname,
+//	   a <slash> character if that pathname did not end with a <slash> character, and the directory operand names
+//	   a directory. If the pathname is null, test if the concatenation of dot, a <slash> character, and the operand
+//	   names a directory. In either case, if the resulting string names an existing directory, set curpath to that
+//	   string and proceed to step 7. Otherwise, repeat this step with the next pathname in CDPATH until all
+//	   pathnames have been tested.
+	else if ((cdpath = concatenate_cdpath(directory)))
+		curpath = cdpath;
+//	6. Set curpath to the directory operand.
+	if (!curpath)
+		curpath = directory;
+//	7. If the -P option is in effect, proceed to step 10. If curpath does not begin with a <slash> character,
+//	   set curpath to the string formed by the concatenation of the value of PWD, a <slash> character if the
+//	   value of PWD did not end with a <slash> character, and curpath.
+//	if (p_option)
+
+
+
+
+//	8.
+
+//	9.
+
+
+
+
+
+//	10. The cd utility shall then perform actions equivalent to the chdir() function called with curpath as the
+//	    path argument. If these actions fail for any reason, the cd utility shall display an appropriate error
+//	    message and the remainder of this step shall not be executed. If the -P option is not in effect, the PWD
+//	    environment variable shall be set to the value that curpath had on entry to step 9 (i.e., before conversion
+//	    to a relative pathname). If the -P option is in effect, the PWD environment variable shall be set to the
+//	    string that would be output by pwd -P. If there is insufficient permission on the new directory, or on any
+//	    parent of that directory, to determine the current working directory, the value of the PWD environment
+//	    variable is unspecified.
+
+
+
+
 /*
 	if ((cd.ret = getfullpath(argv, &cd)))
 		return (cd.ret);
