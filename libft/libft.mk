06ae0bb856e93b04c995717b50e6eef252ef8dc4
Antoine Barthelemy <abarthel@student.42.fr>
2020-05-06 20:55:08 +0200

    fix(cd): need further tests, major bugs fixed

--- libft/libft.mk
+++ libft/libft.mk
@@ -6,7 +6,7 @@
 #    By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/06/25 14:45:47 by abarthel          #+#    #+#              #
-#    Updated: 2020/05/06 10:02:51 by abarthel         ###   ########.fr        #
+#    Updated: 2020/05/06 20:50:40 by abarthel         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -220,6 +220,5 @@ DEPENDS := $(patsubst %.c,%.d,$(addprefix $(SOURCES_PATH), $(SOURCES)))
 
 ARFLAGS := rc
 
-CFLAGS += -Wall -Werror -Wextra -ansi -std=c99 -g3 -D_POSIX_C_SOURCE
-#CFLAGS += -fno-builtin -O2
-CFLASG += -fsanitize=address,undefined
+CFLAGS += -Wall -Werror -Wextra -ansi -std=c99 -g3 -D_POSIX_C_SOURCE -fsanitize=address,undefined
+#-fno-builtin -O2
