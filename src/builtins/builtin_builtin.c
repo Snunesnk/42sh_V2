6e947785f0bd372af99bcea992c097c091f43062
Antoine Barthelemy <abarthel@student.42.fr>
2020-05-08 09:30:27 +0200

    fix(execute_process): adding builtin builtin and modify error builtin_dispatcher

--- src/builtins/builtin_builtin.c
+++ src/builtins/builtin_builtin.c
@@ -3,23 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_builtin.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
+/*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/04/28 13:50:06 by yforeau           #+#    #+#             */
-/*   Updated: 2020/04/28 13:55:23 by yforeau          ###   ########.fr       */
+/*   Created: 2020/05/08 09:13:43 by abarthel          #+#    #+#             */
+/*   Updated: 2020/05/08 09:29:02 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "shell.h"
 #include "builtins.h"
 
-int			cmd_builtin(int argc, char **argv)
+int		cmd_builtin(int argc, char **argv)
 {
-	int	ret;
+	int err;
 
-	(void)argc;
-	ret = e_success;
-	if (argv[1] && (ret = builtins_dispatcher(&argv[1])) != e_command_not_found)
-		return (ret);
-	else
-		return (g_errordesc[psherror(e_no_builtin, argv[1], e_cmd_type)].code);
+	if (argc >= 2)
+	{
+		err = builtins_dispatcher(&argv[1]);
+		if (err == e_no_builtin)
+		{
+			psherror(e_no_builtin, argv[1], e_cmd_type);
+			return (g_errordesc[err].code);
+		}
+		return (err);
+	}
+	return (0);
 }
