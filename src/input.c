ff61d4a2bbaf1409d150bc7ac6ac743a3ff1f179
Antoine Barthelemy <abarthel@student.42.fr>
2020-05-09 15:51:40 +0200

    fix(subprmpt): prep for refactoring heredoc and parser recall subprompt

--- src/input.c
+++ src/input.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 16:48:52 by abarthel          #+#    #+#             */
-/*   Updated: 2020/05/09 13:33:13 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/05/09 15:50:12 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -119,31 +119,3 @@ char		*append_line(char **last_lines, char *input)
 	}
 	return (input);
 }
-
-//char			*get_input(char *prompt, int close_quotes)
-//{
-//	char	*input;
-//	int		first_iter;
-//	char	*last_lines;
-//
-//	input = NULL;
-//	first_iter = 1;
-//	last_lines = NULL;
-//	while (first_iter || last_lines)
-//	{
-//		if (g_shell_is_interactive)
-//			input = ft_readline(first_iter ? prompt : "> ");
-//		else if (get_stdin(STDIN_FILENO, &input) < 0)
-//			break ;
-//		if (input && close_quotes)
-//			input = append_line(&last_lines, input);
-//		first_iter = 0;
-//	}
-//	if (!input && last_lines)
-//		input = last_lines;
-//	else
-//		ft_memdel((void **)&last_lines);
-//	if (g_shell_is_interactive)
-//		add_hentry(input, ft_strlen(input));
-//	return (input);
-//}
