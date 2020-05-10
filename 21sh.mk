908afde74b55053cc9f88bc349123122deadb0e9
Simon Nunes <snunes@student.42.fr>
2020-05-10 12:50:23 +0200

    fix history expansion display

--- 21sh.mk
+++ 21sh.mk
@@ -6,7 +6,7 @@
 #    By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/06/25 14:45:47 by abarthel          #+#    #+#              #
-#    Updated: 2020/05/09 11:30:27 by abarthel         ###   ########.fr        #
+#    Updated: 2020/05/10 12:16:05 by snunes           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -201,4 +201,4 @@ LDFLAGS += $(DEBUG)
 CFLAGS += -Wall -Wextra -Werror -g3 -D_POSIX_JOB_CONTROL -D_POSIX_C_SOURCE=200809L $(DEBUG)
 #CFLAGS += -fno-builtin -O2
 
-DEBUG += -fsanitize=address,undefined
+#DEBUG += -fsanitize=address,undefined
