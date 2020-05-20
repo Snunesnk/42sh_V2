6c7033b27eac40038675acd4678e8b361eefd715
Simon Nunes <snunes@student.42.fr>
2020-05-20 14:37:25 +0200

    rm file and fix hash nb_called

--- src/hash_table_utils.c
+++ src/hash_table_utils.c
@@ -6,7 +6,7 @@
 /*   By: snunes <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/27 14:37:21 by snunes            #+#    #+#             */
-/*   Updated: 2020/05/20 14:29:34 by snunes           ###   ########.fr       */
+/*   Updated: 2020/05/20 14:36:52 by snunes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,10 @@ t_hash_table	*find_occurence(char *name)
 	while (tmp && !ft_strequ(tmp->command_name, name))
 		tmp = tmp->next;
 	if (tmp && access(tmp->command_path, F_OK))
+	{
+		tmp->nb_called++;
 		return (NULL);
+	}
 	return (tmp);
 }
 
