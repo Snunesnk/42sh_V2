925cbf43378dfafb305f8ae76006e115c4676359
Antoine Barthelemy <abarthel@student.42.fr>
2020-05-21 17:05:10 +0200

    fix(heredoc expansions): expansions are later done in redir to get updated variables when executing

--- src/redir_less.c
+++ src/redir_less.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 12:36:35 by abarthel          #+#    #+#             */
-/*   Updated: 2020/05/19 11:15:41 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/05/21 17:03:36 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ t_redirection	*type_dless_redirection(t_list **lst, int io_nb)
 	r->instruction = IOHERE;
 	(*lst) = (*lst)->next;
 	r->redirector.hereword = ft_strdup(get_tokvalue(*lst));
+	expand_filename(r, &r->redirector.hereword); // Test for heredoc expansions
 	(*lst) = (*lst)->next;
 	return (r);
 }
