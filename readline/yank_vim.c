90c36580ee38658691f7451fc462aead3da9517e
Antoine Barthelemy <abarthel@student.42.fr>
2020-05-24 13:19:01 +0200

    feat(vim): y-b

--- readline/yank_vim.c
+++ readline/yank_vim.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/24 10:26:06 by abarthel          #+#    #+#             */
-/*   Updated: 2020/05/24 12:54:07 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/05/24 13:17:36 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,18 +34,16 @@ void	cp_prev_wd(void)
 
 	if (g_line.c_pos != 0)
 	{
-		if (ft_ispunct(g_line.line[g_line.c_pos - 1]))
-			return;
-		if (g_clip.str != NULL)
-			free(g_clip.str);
-		start = g_line.c_pos;
+		start = g_line.c_pos + 1;
 		while (start && g_line.line[start - 1] == ' '
 				&& !ft_ispunct(g_line.line[start - 1]))
 			--start;
 		while (start && g_line.line[start - 1] != ' '
 				&& !ft_ispunct(g_line.line[start - 1]))
 			--start;
-		g_clip.l = g_line.c_pos - start;
+		if (g_clip.str != NULL)
+			free(g_clip.str);
+		g_clip.l = g_line.c_pos + 1 - start;
 		g_clip.str = ft_strndup(&(g_line.line[start]), g_clip.l);
 	}
 
