21b0514caf043fad9d4994c0f52cf946005a65ed
Simon Nunes <snunes@student.42.fr>
2020-05-24 12:15:11 +0200

    Fix x and X cmd with vim binding

--- readline/bind.h
+++ readline/bind.h
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 17:20:25 by abarthel          #+#    #+#             */
-/*   Updated: 2020/05/24 00:33:33 by snunes           ###   ########.fr       */
+/*   Updated: 2020/05/24 11:58:10 by snunes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,5 +105,7 @@ void			vim_cmd_cursor_r(void);
 void			vim_cmd_cursor_l(void);
 void			goto_first_non_blank(void);
 void			goto_n_char(void);
+void			vim_delete(void);
+void			vim_backspace(void);
 
 #endif
--- readline/vim8.c
+++ readline/vim8.c
@@ -6,15 +6,61 @@
 /*   By: snunes <snunes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/23 17:34:10 by snunes            #+#    #+#             */
-/*   Updated: 2020/05/23 17:45:47 by snunes           ###   ########.fr       */
+/*   Updated: 2020/05/24 12:12:17 by snunes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_readline.h"
+#include "error.h"
 
 void	goto_n_char(void)
 {
 	g_line.c_pos = g_vim_cmd_count;
 	if (g_line.c_pos > g_line.len)
 		g_line.c_pos = g_line.len;
+}
+
+void	vim_delete(void)
+{
+	int	len;
+
+	if (g_line.len == 0)
+		return ;
+	if (!g_vim_cmd_count)
+		g_vim_cmd_count = 1;
+	if ((len = g_line.c_pos + g_vim_cmd_count) > g_line.len)
+		len = g_line.len;
+	len -= g_line.c_pos;
+	if (g_clip.str)
+		free(g_clip.str);
+	if (!(g_clip.str = ft_strndup(g_line.line + g_line.c_pos, len)))
+	{
+		psherror(e_cannot_allocate_memory, g_progname, e_cmd_type);
+		return ;
+	}
+	while (len--)
+		rl_delete();
+	if (g_line.c_pos == g_line.len)
+		cursor_l();
+}
+
+void	vim_backspace(void)
+{
+	int	len;
+
+	if (g_line.len == 0 || g_line.c_pos == 0)
+		return ;
+	if (!g_vim_cmd_count)
+		g_vim_cmd_count = 1;
+	if ((len = g_line.c_pos - g_vim_cmd_count) < 0)
+		len = 0;
+	if (g_clip.str)
+		free(g_clip.str);
+	if (!(g_clip.str = ft_strndup(g_line.line + len, g_line.c_pos - len)))
+	{
+		psherror(e_cannot_allocate_memory, g_progname, e_cmd_type);
+		return ;
+	}
+	while (g_line.c_pos > len)
+		rl_backspace();
 }
