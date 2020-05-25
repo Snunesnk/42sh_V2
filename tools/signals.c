51411de0aa3a825c519ab2d71bf4066e2976013c
Antoine Barthelemy <abarthel@student.42.fr>
2020-05-25 15:52:23 +0200

    fix(test): adding tests for signals

--- tools/signals.c
+++ tools/signals.c
@@ -1,29 +1,139 @@
 cat &
-pkill -HUP
-pkill -INT
-pkill -QUIT
-pkill -ILL
-pkill -ABRT
-pkill -FPE
-pkill -KILL
-pkill -SEGV
-pkill -PIPE
-pkill -ALRM
-pkill -TERM
-pkill -USR1
-pkill -USR2
-pkill -CHLD
-pkill -CONT
-pkill -STOP
-pkill -TSTP
-pkill -TTIN
-pkill -TTOU
-pkill -BUS
-pkill -POLL
-pkill -PROF
-pkill -SYS
-pkill -TRAP
-pkill -URG
-pkill -VTALRM
-pkill -XCPU
-pkill -XFSZ
+pkill -HUP cat
+fg
+echo $?
+
+cat &
+pkill -INT  cat
+fg
+echo $?
+
+cat &
+pkill -QUIT  cat
+fg
+echo $?
+
+cat &
+pkill -ILL  cat
+fg
+echo $?
+
+cat &
+pkill -ABRT  cat
+fg
+echo $?
+
+cat &
+pkill -FPE  cat
+fg
+echo $?
+
+cat &
+pkill -KILL  cat
+fg
+echo $?
+
+cat &
+pkill -SEGV  cat
+fg
+echo $?
+
+cat &
+pkill -PIPE  cat
+fg
+echo $?
+
+cat &
+pkill -ALRM  cat
+fg
+echo $?
+
+cat &
+pkill -TERM  cat
+fg
+echo $?
+
+cat &
+pkill -USR1  cat
+fg
+echo $?
+
+cat &
+pkill -USR2  cat
+fg
+echo $?
+
+cat &
+pkill -CHLD  cat
+fg
+echo $?
+
+cat &
+pkill -CONT  cat
+fg
+echo $?
+
+cat &
+pkill -STOP  cat
+fg
+echo $?
+
+cat &
+pkill -TSTP  cat
+fg
+echo $?
+
+cat &
+pkill -TTIN  cat
+fg
+echo $?
+
+cat &
+pkill -TTOU  cat
+fg
+echo $?
+
+cat &
+pkill -BUS  cat
+fg
+echo $?
+
+cat &
+pkill -POLL  cat
+fg
+echo $?
+
+cat &
+pkill -PROF  cat
+fg
+echo $?
+
+cat &
+pkill -SYS  cat
+fg
+echo $?
+
+cat &
+pkill -TRAP  cat
+fg
+echo $?
+
+cat &
+pkill -URG  cat
+fg
+echo $?
+
+cat &
+pkill -VTALRM  cat
+fg
+echo $?
+
+cat &
+pkill -XCPU  cat
+fg
+echo $?
+
+cat &
+pkill -XFSZ  cat
+fg
+echo $?
