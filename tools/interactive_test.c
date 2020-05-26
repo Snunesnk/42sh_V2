4bb81087d29a95762c878071f27e2179f1e9f062
Simon Nunes <snunes@student.42.fr>
2020-05-26 16:31:41 +0200

    Fix display fd of CTRL-C newline, fix fd of get_cursor_position, and finish interactive_tester

--- tools/interactive_test.c
+++ tools/interactive_test.c
@@ -23,30 +23,110 @@
 #include <signal.h>
 #define _GNU_SOURCE
 
-char	cmd[][999] =
-{
-	"hola!\n"
-};
+//To read dir files
+#include <dirent.h>
+
+//test file extension
+#define TEST_EXT ".test"
 
 // This function send a cmd to a process with ioctl, storing the
 // input into the queue of desired process.
 static void	send_input(char *input, int fd)
 {
-	int			i;
+	int				i;
+	struct pollfd	fds;
+	int				status;
+	char			stop = 3;
 
+	//init struct for poll, to wait till input in process become available
+	fds.fd = fd;
+	fds.events = (POLLIN);
+	fds.revents = 0;
+
+	printf("input: |%s|\n", input);
 	i = 0;
 	while (input[i])
 	{
 		if (ioctl(fd, TIOCSTI, &(input[i])) < 0)
 		{
-			perror("ioctl 1");
+			perror("ioctl");
 			exit(1);
 		}
 		i++;
 		// Wait between each sent char, otherwise process's read will return more than
 		// one char, causing the input to not behave properly.
-		usleep(100000);
+		usleep(1000);
 	}
+	usleep(1000);
+	//Wait for the input to become available, or timeout after 3 seconds
+	//if process didn't respond in time, quit. Not great, but don't know how to make it better now.
+	if (!strchr(input, '\n'))
+		return ;
+	while ((status = poll(&fds, 1, 3000)) <= 0)
+	{
+		if (status == 0)
+		{
+			if (ioctl(fd, TIOCSTI, &stop) < 0)
+			{
+				perror("ioctl");
+				exit(1);
+			}
+			usleep(1000);
+			break ;
+		}
+	}
+}
+
+static void	open_and_exec_file(char *file, int fd)
+{
+	FILE	*fp;
+
+	if (!(fp = fopen(file, "r")))
+	{
+		perror("fopen");
+		return ;
+	}
+
+	char	line[1024];
+
+	while (fgets(line, 1023, fp) != NULL)
+		send_input(line, fd);
+	fclose(fp);
+}
+
+static int	ft_strequ(char *s1, char *s2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (0);
+	while (s1[i] == s2[j] && s1[i] && s2[i])
+	{
+		i++;
+		j++;
+	}
+	return (s1[i] == s2[j]);
+}
+
+static void	exec_all_tests(int fd)
+{
+	DIR				*dirp;
+	struct dirent	*file;
+	int				ext_name = strlen(TEST_EXT);
+	int				name_len;
+
+	dirp = opendir(".");
+	while (file = readdir(dirp))
+	{
+		name_len = strlen(file->d_name);
+		if (name_len > ext_name && ft_strequ(file->d_name + name_len - ext_name, TEST_EXT) \
+					&& file->d_name[0] != '.')
+			open_and_exec_file(file->d_name, fd);
+	}
+	closedir(dirp);
 }
 
 int			main(int argc, char **argv)
@@ -83,32 +163,17 @@ int			main(int argc, char **argv)
 	int				pid = atoi(argv[1]);
 	int				i = 2;
 	int				status;
-	struct pollfd	fds;
-
-	//init struct for poll, to wait till input in process become available
-	fds.fd = fd;
-	fds.events = (POLLIN);
-	fds.revents = 0;
 	// If the user send command through cmd line, execute them.
-	while (argv[i])
+	if (argc > 2)
 	{
-		send_input(argv[i], fd);
-		//Wait for the input to become available, or timeout after 30 seconds
-		while ((status = poll(&fds, 1, 30000)) < 0);
-		//if process didn't respond in time, kill the launch cmd.
-		if (status == 0)
-			kill(pid, SIGINT);
-		i++;
+		while (argv[i])
+		{
+			open_and_exec_file(argv[i], fd);
+			i++;
+		}
 	}
-	i = 0;
-	// Execute all cmd stored in cmd[][].
-	while (i < 1)
-	{
-		send_input(cmd[i], fd);
-		while ((status = poll(&fds, 1, 30000)) < 0);
-		if (status == 0)
-			kill(pid, SIGINT);
-		i++;
-	}
+	// else exec all *.test in current directory
+	else
+		exec_all_tests(fd);
 	close(fd);
 }
